37249fdcd4526a9c853c564f6195cf70a4764a0e
flashburst <82953782+flashburst@users.noreply.github.com>
2021-12-11 12:54:11 +0530

    Add tailwind CSS

---
+++ src/pages/_document.jsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-poppins text-black text-para">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ tailwind.config.js
@@ -0,0 +1,102 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./lib/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      xl: "0.625rem",
+      "2xl": "0.75rem",
+      "3xl": "1rem",
+      "4xl": "1.5rem",
+      full: "9999px",
+    },
+    boxShadow: {
+      card: "0px 6px 8px 0px rgba(0, 0, 0, 0.1)",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#01052D",
+    },
+    fontSize: {
+      h1: ["33px", "48px"],
+      h2: ["28px", "40px"],
+      h3: ["23px", "32px"],
+      h4: ["19px", "24px"],
+      h5: ["16px", "24px"],
+      h6: [
+        "16px",
+        {
+          letterSpacing: "0.05em",
+          lineHeight: "24px",
+        },
+      ],
+      sm: ["14px", "16px"],
+      para: ["16px", "28px"],
+      body: ["16px", "32px"],
+      cta: [
+        "15px",
+        {
+          letterSpacing: "0.05em",
+          lineHeight: "16px",
+        },
+      ],
+    },
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+      sora: "Sora, sans-serif",
+    },
+    extend: {
+      spacing: {
+        /* 72px */
+        18: "4.5rem",
+      },
+      colors: {
+        primary: "#4E7DD9",
+        gray: {
+          /* background */
+          bg: "#F1F3F6",
+        },
+        white: {
+          /* background */
+          bg: "#FEFEFF",
+          /* text */
+          fg: "#EEEEEE",
+        },
+        dimmed: {
+          /* text */
+          fg: "#9B9B9B",
+          /* card text*/
+          card: "#7398C0",
+        },
+        ash: {
+          fg: "#89A0C2",
+          /* background (gray) */
+          bg: "#F1F3F6",
+          /* Neutral Brand */
+          neutral: "#364253",
+          /* Background (Brand, Neutral) */
+          brand: "#DEEAF6",
+          /* TextInput (Border Color) */
+          border: "#B0C4DB",
+          /* Background (Secondary Button) */
+          secondary: "#DAE2EB",
+        },
+        teal: {
+          /* light */
+          light: "#CEEBED",
+          neutral: "#21AD8C",
+        },
+        orange: {
+          neutral: "#FA5C2F",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
