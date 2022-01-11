9eaaf483357bd1a2d2ed84eb4ef229228a6b5d9b
savinnpm <savin@neptunemutual.com>
2022-01-11 15:51:48 +0545

    fixed navigation menu slectection

--- src/components/UI/organisms/header/index.jsx
+++ src/components/UI/organisms/header/index.jsx
@@ -5,15 +5,17 @@ import { useRouter } from "next/router";
 
 const navigation = [
   { name: "Home", href: "/" },
-  { name: "Pool", href: "/pools/bond" },
-  { name: "My Policies", href: "/my-policies/active" },
+  /* { name: "Pool", href: "/pools/bond" }, */
+  { name: "Pool", href: "/pools/" },
+  { name: "My Policies", href: "/my-policies/" },
   { name: "My Liquidity", href: "/my-liquidity" },
-  { name: "Reporting", href: "/reporting/active" },
+  { name: "Reporting", href: "/reporting/" },
 ];
 
 export const Header = () => {
   const router = useRouter();
 
+  console.log(router.pathname);
   return (
     <header className="bg-black text-EEEEEE">
       <nav className="max-w-full mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
@@ -26,12 +28,25 @@ export const Header = () => {
               {navigation.map((link) => {
                 return (
                   <Link key={link.name} href={link.href}>
+                    {/* <a
+                      className={classNames(
+                        "text-sm py-7 border-b-4",
+                        router.pathname.startsWith(link.href)
+                          ? "border-4E7DD9 text-4E7DD9 font-semibold"
+                          : "border-transparent text-999BAB"
+                      )}
+                    > */}
                     <a
                       className={classNames(
                         "text-sm py-7 border-b-4",
-                        router.pathname == link.href
+                        link.name !== "Home" &&
+                          router.pathname.slice(1).includes(link.href.slice(1))
                           ? "border-4E7DD9 text-4E7DD9 font-semibold"
-                          : "border-transparent text-999BAB"
+                          : "border-transparent text-999BAB",
+                        link.name == "Home" &&
+                          (router.pathname == link.href ||
+                            router.pathname.includes("cover")) &&
+                          "!border-4E7DD9 !text-4E7DD9 font-semibold"
                       )}
                     >
                       {link.name}
