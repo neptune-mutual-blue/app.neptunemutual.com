8ce3f507405de8c57a35008d84b9ade856ef34bf
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-11 10:22:49 +0530

    Update network resolution

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,18 +1,22 @@
 function getChainIdFromDNS() {
-    // window.location.host - subdomain.domain.com
-    const parts = window.location.host.split(".");
+  // window.location.host - subdomain.domain.com
+  const parts = window.location.host.split(".");
 
-    switch (parts[0]) {
-      case "mumbai":
-        return "80001";
-      case "bsctest":
-        return "97";
-      case "app":
-        return "56";
-  
-      default:
-        return "3";
-    }
+  switch (parts[0]) {
+    case "mumbai":
+      return "80001";
+    case "bsctest":
+      return "97";
+    case "app":
+      return "1";
+    case "bsc":
+      return "56";
+    case "polygon":
+      return "137";
+
+    default:
+      return "3";
+  }
 }
 
 export const getNetworkId = () => parseInt(getChainIdFromDNS(), 10);
