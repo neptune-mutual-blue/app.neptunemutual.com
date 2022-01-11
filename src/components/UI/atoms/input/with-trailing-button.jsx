442c6c7a25abcd1454df3ac2b1e4423b07c2982d
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-11 16:45:13 +0530

    Add validation for policy purchase input

--- src/components/UI/atoms/input/with-trailing-button.jsx
+++ src/components/UI/atoms/input/with-trailing-button.jsx
@@ -1,6 +1,12 @@
+import { classNames } from "@/utils/classnames";
 import { useState, useEffect, useRef } from "react";
 
-export const InputWithTrailingButton = ({ inputProps, unit, buttonProps }) => {
+export const InputWithTrailingButton = ({
+  inputProps,
+  unit,
+  buttonProps,
+  error,
+}) => {
   const ref = useRef(null);
 
   const [width, setWidth] = useState();
@@ -22,7 +28,12 @@ export const InputWithTrailingButton = ({ inputProps, unit, buttonProps }) => {
   return (
     <div className="relative text-black text-h4 w-full">
       <input
-        className="bg-white block w-full py-6 pl-6 pr-40 rounded-lg overflow-hidden border border-B0C4DB focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-4e7dd9"
+        className={classNames(
+          "bg-white block w-full py-6 pl-6 pr-40 rounded-lg overflow-hidden border",
+          error
+            ? "border-FA5C2F focus:outline-none focus-visible:ring-2 focus-visible:ring-FA5C2F"
+            : "border-B0C4DB focus:outline-none focus-visible:ring-2 focus-visible:ring-4e7dd9"
+        )}
         style={{ paddingRight: `${width || 64}px` }}
         {...inputProps}
       />
