8a9219e1d17fccfdf3508411455c05099f53c8cf
Milan Panta <milan@neptunemutual.com>
2022-01-13 13:26:34 +0545

    feat: use SDK to provide liquidity

--- src/components/UI/organisms/cover-form/add-liquidity.jsx
+++ src/components/UI/organisms/cover-form/add-liquidity.jsx
@@ -1,27 +1,112 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+
+import { registry } from "@neptunemutual/sdk";
+
+import { useWeb3React } from "@web3-react/core";
+import { getERC20Balance } from "@/utils/blockchain/getERC20Balance";
+import { getERC20Allowance } from "@/utils/blockchain/getERC20Allowance";
+import {
+  convertToUnits,
+  convertFromUnits,
+  isGreater,
+  isValidNumber,
+} from "@/utils/bn";
+
 import { useConstants } from "@/components/pages/cover/useConstants";
 import { OutlinedButton } from "@/components/UI/atoms/button/outlined";
 import { TokenAmountInput } from "@/components/UI/organisms/token-amount-input";
 import { RegularButton } from "@/components/UI/atoms/button/regular";
 import { ReceiveAmountInput } from "@/components/UI/organisms/receive-amount-input";
 import { UnlockDate } from "@/components/UI/organisms/unlock-date";
+
 import { useToast } from "@/lib/toast/context";
+import { getProviderOrSigner } from "@/lib/connect-wallet/utils/web3";
+
 import OpenInNewIcon from "@/icons/open-in-new";
 
 const ERROR_TIMEOUT = 30000; // 30 seconds
 
-export const CoverFormAddLiquidity = () => {
+export const CoverFormAddLiquidity = ({
+  assuranceTokenAddress,
+  assuranceTokenSymbol,
+  coverKey,
+}) => {
   const router = useRouter();
   const toast = useToast();
+  const { fees, maxValue } = useConstants();
+
+  const { library, account, chainId } = useWeb3React();
 
   const [value, setValue] = useState();
   const [receiveAmount, setReceiveAmount] = useState();
-  const { fees, maxValue } = useConstants();
+  const [balance, setBalance] = useState();
+  const [allowance, setAllowance] = useState();
+  const [spender, setSpender] = useState();
+  const [approving, setApproving] = useState();
+  const [providing, setProviding] = useState();
+
+  useEffect(() => {
+    if (!chainId || !account) return;
+
+    let ignore = false;
+
+    getERC20Balance(assuranceTokenAddress, library, account, chainId)
+      .then((bal) => {
+        if (ignore) return;
+        setBalance(bal);
+      })
+      .catch((e) => {
+        console.error(e);
+        if (ignore) return;
+      });
+
+    return () => (ignore = true);
+  }, [account, chainId, library, assuranceTokenAddress]);
+
+  useEffect(() => {
+    if (!chainId || !account) return;
+
+    let ignore = false;
+
+    getERC20Allowance(spender, assuranceTokenAddress, library, account, chainId)
+      .then((bal) => {
+        if (ignore) return;
+        setAllowance(bal);
+      })
+      .catch((e) => {
+        console.error(e);
+        if (ignore) return;
+      });
+
+    return () => (ignore = true);
+  }, [account, chainId, library, assuranceTokenAddress, spender]);
+
+  useEffect(() => {
+    if (!chainId || !account) return;
+
+    let ignore = false;
+
+    const signerOrProvider = getProviderOrSigner(library, account, chainId);
+
+    registry.Vault.getAddress(chainId, coverKey, signerOrProvider)
+      .then((addr) => {
+        if (ignore) return;
+        setSpender(addr);
+      })
+      .catch((e) => {
+        console.error(e);
+        if (ignore) return;
+      });
+
+    return () => (ignore = true);
+  }, [account, chainId, library, assuranceTokenAddress]);
 
   const handleChooseMax = () => {
-    setValue(maxValue);
-    setReceiveAmount(parseFloat(0.99 * maxValue).toFixed(2));
+    if (!balance) {
+      return;
+    }
+    setValue(convertFromUnits(balance).toString());
   };
 
   const handleChange = (e) => {
@@ -43,18 +128,86 @@ export const CoverFormAddLiquidity = () => {
     });
   };
 
+  const checkAllowance = async () => {
+    await getERC20Allowance(
+      spender,
+      assuranceTokenAddress,
+      library,
+      account,
+      chainId
+    )
+      .then((bal) => {
+        setAllowance(bal);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  const handleApprove = async () => {
+    try {
+      setApproving(true);
+      const signerOrProvider = getProviderOrSigner(library, account, chainId);
+
+      let tx = await liquidity.approve(
+        chainId,
+        coverKey,
+        { amount: convertToUnits(value).toString() },
+        signerOrProvider
+      );
+
+      await tx.result.wait();
+
+      setApproving(false);
+      checkAllowance();
+    } catch (error) {
+      setApproving(false);
+    }
+  };
+
+  const handleProvide = async () => {
+    try {
+      setProviding(true);
+
+      const signerOrProvider = getProviderOrSigner(library, account, chainId);
+      const amount = convertToUnits(value).toString();
+
+      const tx = await liquidity.add(
+        chainId,
+        coverKey,
+        amount,
+        signerOrProvider
+      );
+      await tx.result.wait();
+
+      setProviding(false);
+    } catch (error) {
+      setProviding(false);
+    }
+  };
+
   if (!fees && !maxValue) {
     return <>loading...</>;
   }
+
+  const canProvideLiquidity =
+    value && !isValidNumber(value) && isGreater(allowance || "0", value || "0");
+  const isError =
+    value &&
+    (!isValidNumber(value) ||
+      isGreater(convertToUnits(value || "0"), balance || "0"));
 
   return (
     <div className="max-w-md">
       <div className="pb-16">
         <TokenAmountInput
-          labelText={"Amount you wish to provide"}
+          labelText={"Enter Amount you wish to provide"}
           onInput={handleChange}
           handleChooseMax={handleChooseMax}
-          tokenSymbol={"DAI"}
+          error={isError}
+          tokenAddress={assuranceTokenAddress}
+          tokenSymbol={assuranceTokenSymbol}
+          tokenBalance={balance}
           inputId={"cover-amount"}
           inputValue={value}
         />
@@ -73,13 +226,23 @@ export const CoverFormAddLiquidity = () => {
         <UnlockDate dateValue="September 22, 2021 12:34:00 PM UTC" />
       </div>
 
-      <RegularButton
-        onClick={handleProvideLiquidity}
-        disabled={!value}
-        className="w-full mt-8 p-6 text-h6 uppercase font-semibold"
-      >
-        Provide Liquidity
-      </RegularButton>
+      {!canProvideLiquidity ? (
+        <RegularButton
+          disabled={isError || approving}
+          className="w-full mt-8 p-6 text-h6 uppercase font-semibold"
+          onClick={handleApprove}
+        >
+          {approving ? "Approving..." : <>Provide Liquidity</>}
+        </RegularButton>
+      ) : (
+        <RegularButton
+          disabled={isError || providing}
+          className="w-full mt-8 p-6 text-h6 uppercase font-semibold"
+          onClick={handleProvide}
+        >
+          {providing ? "Adding Liquidity..." : <>Provide Liquidity</>}
+        </RegularButton>
+      )}
 
       <div className="mt-16">
         <OutlinedButton className="rounded-big" onClick={() => router.back()}>
