f073aeb57d1e431cde876bb56372be45d64161b8
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-14 13:04:58 +0530

    Update Manifest

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,53 +8,53 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/apple-icon-57x57.png"
+            href="/icons/apple-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/apple-icon-60x60.png"
+            href="/icons/apple-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/apple-icon-72x72.png"
+            href="/icons/apple-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/apple-icon-76x76.png"
+            href="/icons/apple-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/apple-icon-114x114.png"
+            href="/icons/apple-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/apple-icon-120x120.png"
+            href="/icons/apple-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/apple-icon-144x144.png"
+            href="/icons/apple-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/apple-icon-152x152.png"
+            href="/icons/apple-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-icon-180x180.png"
+            href="/icons/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/android-icon-192x192.png"
+            href="/icons/android-icon-192x192.png"
           />
           <link
             rel="icon"
@@ -76,7 +76,10 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#01052D" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta
+            name="msapplication-TileImage"
+            content="/icons/ms-icon-144x144.png"
+          />
           <meta name="theme-color" content="#01052D" />
         </Head>
         <body className="font-poppins text-black text-para bg-f1f3f6">
