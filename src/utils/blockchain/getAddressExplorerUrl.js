6c36fe507e3583e484c3862238592b011dbdecf6
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-14 12:49:50 +0530

    Add favicon

--- src/utils/blockchain/getAddressExplorerUrl.js
+++ src/utils/blockchain/getAddressExplorerUrl.js
@@ -1,10 +1,5 @@
 import { explorer } from "@/lib/connect-wallet/config/chains";
 
 export const getAddressExplorerUrl = (address, networkId) => {
-  console.log("getAddressExplorerUrl", [
-    address,
-    networkId,
-    explorer.address[networkId],
-  ]);
   return explorer.address[networkId] + address;
 };
