e6e96d594a2a2215a9b93dcfb8d5279db470dbc2
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-18 07:43:56 +0530

    Fix connect wallet issue

--- src/components/UI/organisms/AppWrapper.jsx
+++ src/components/UI/organisms/AppWrapper.jsx
@@ -11,14 +11,22 @@ export const useAppContext = () => useContext(AppContext);
 export const AppWrapper = ({ children }) => {
   const [networkId, setNetworkId] = useState();
 
-  useEagerConnect(networkId);
-  useInactiveListener(networkId);
-
   useEffect(() => {
     setNetworkId(getNetworkId());
   }, []);
 
   return (
-    <AppContext.Provider value={{ networkId }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ networkId }}>
+      {networkId && <PostNetworkIdLoad networkId={networkId} />}
+      {children}
+    </AppContext.Provider>
   );
+};
+
+// This component makes sure that given hooks are only executed once after networkId is loaded
+const PostNetworkIdLoad = ({ networkId }) => {
+  useEagerConnect(networkId);
+  useInactiveListener(networkId);
+
+  return null;
 };
