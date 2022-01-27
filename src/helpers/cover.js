9a474ce0efb0cc7011d193315ed24e3b37e46f63
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-28 05:11:13 +0530

    Add empty cover image

--- src/helpers/cover.js
+++ src/helpers/cover.js
@@ -7,7 +7,7 @@ export const getCoverImgSrc = (coverInfo) => {
   try {
     return `/images/covers/${parseBytes32String(coverInfo?.key)}.png`;
   } catch (error) {
-    return `/images/covers/clearpool.png`;
+    return `/images/covers/empty.png`;
   }
 };
 
