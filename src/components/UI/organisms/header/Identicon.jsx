ad4c9c343d7cecb2a32f07d920a600dfcb553c0d
flashburst <82953782+flashburst@users.noreply.github.com>
2022-02-14 11:15:37 +0530

    Update identicon

--- src/components/UI/organisms/header/Identicon.jsx
+++ src/components/UI/organisms/header/Identicon.jsx
@@ -1,29 +1,37 @@
 export default function Identicon() {
   return (
-    <div className="flex justify-center items-center h-4 w-4 rounded-full">
-      <svg x="0" y="0" width="16" height="16">
+    <div
+      style={{
+        borderRadius: "50%",
+        overflow: "hidden",
+        width: "24px",
+        height: "24px",
+        background: "rgb(1, 109, 142)",
+      }}
+    >
+      <svg viewBox="0 0 32 32" x="0" y="0" width="24" height="24">
         <rect
           x="0"
           y="0"
-          width="16"
-          height="16"
-          transform="translate(-0.9528795077844953 3.3315554290796165) rotate(150.9 8 8)"
+          width="32"
+          height="32"
+          transform="translate(-1.9 6.6) rotate(150.9 16 16)"
           fill="#C81429"
         ></rect>
         <rect
           x="0"
           y="0"
-          width="16"
-          height="16"
-          transform="translate(7.306835344999555 -3.045859603554785) rotate(429.8 8 8)"
+          width="32"
+          height="32"
+          transform="translate(14.6 -6.0) rotate(429.8 16 16)"
           fill="#F2CE02"
         ></rect>
         <rect
           x="0"
           y="0"
-          width="16"
-          height="16"
-          transform="translate(-1.6016702397927556 11.465374488801697) rotate(199.2 8 8)"
+          width="32"
+          height="32"
+          transform="translate(-3.2 22.9) rotate(199.2 16 16)"
           fill="#F97101"
         ></rect>
       </svg>
