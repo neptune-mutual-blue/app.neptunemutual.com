e029366b01a300c2b3fca7025d6d8ad17d913ee1
rupak-nm <rupak@neptunemutual.com>
2022-02-17 10:02:10 +0545

    close button added to info banner

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,9 +1,12 @@
+import CloseIcon from "@/icons/CloseIcon";
 import { NetworkNames } from "@/lib/connect-wallet/config/chains";
 import { testnetChainIds } from "@/src/config/environment";
 import { useAppContext } from "@/src/context/AppWrapper";
+import { useState } from "react";
 
 export const Banner = () => {
   const { networkId } = useAppContext();
+  const [show, setShow] = useState(true);
 
   const isTestnet = testnetChainIds.indexOf(networkId) > -1;
 
@@ -11,8 +14,14 @@ export const Banner = () => {
     return null;
   }
 
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  if (!show) return <></>;
+
   return (
-    <div className="bg-4e7dd9 text-white text-sm text-center">
+    <div className="relative bg-4e7dd9 text-white text-sm text-center">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <p className="ml-3">
           You are using {NetworkNames[networkId]}. Please{" "}
@@ -27,6 +36,12 @@ export const Banner = () => {
           to get testnet tokens.
         </p>
       </div>
+      <button
+        onClick={handleClose}
+        className="absolute right-[5%] top-1/2 transform -translate-y-1/2"
+      >
+        <CloseIcon className="w-5 h-5" />
+      </button>
     </div>
   );
 };
