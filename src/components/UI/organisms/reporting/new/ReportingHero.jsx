e811a127661d5a87c57c4003175595635d28a9d5
rupak-nm <rupak@neptunemutual.com>
2022-02-18 15:36:12 +0545

    reporting breadcrumb updated

--- src/components/UI/organisms/reporting/new/ReportingHero.jsx
+++ src/components/UI/organisms/reporting/new/ReportingHero.jsx
@@ -4,24 +4,40 @@ import { Hero } from "@/components/UI/molecules/Hero";
 import { CoverProfileInfo } from "@/components/common/CoverProfileInfo";
 import { getCoverImgSrc, getParsedKey } from "@/src/helpers/cover";
 
-export const ReportingHero = ({ coverInfo }) => {
+export const ReportingHero = ({ coverInfo, reportStatus }) => {
   const imgSrc = getCoverImgSrc(coverInfo);
 
   const parsedCoverKey = getParsedKey(coverInfo.key);
-
   return (
     <Hero>
       <Container className="px-2 py-20">
         <BreadCrumbs
-          pages={[
-            { name: "Home", href: "/", current: false },
-            {
-              name: coverInfo?.coverName,
-              href: `/cover/${parsedCoverKey}/options`,
-              current: false,
-            },
-            { name: "Reporting", href: "#", current: true },
-          ]}
+          pages={
+            reportStatus
+              ? [
+                  { name: "Home", href: "/", current: false },
+                  {
+                    name: "Reporting",
+                    href: `/reporting/${
+                      reportStatus.resolved ? "resolved" : "active"
+                    }`,
+                    current: false,
+                  },
+                  {
+                    name: coverInfo?.coverName,
+                    current: true,
+                  },
+                ]
+              : [
+                  { name: "Home", href: "/", current: false },
+                  {
+                    name: coverInfo?.coverName,
+                    href: `/cover/${parsedCoverKey}/options`,
+                    current: false,
+                  },
+                  { name: "Reporting", href: "#", current: true },
+                ]
+          }
         />
         <div className="flex">
           <CoverProfileInfo
