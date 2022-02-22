bd840e98ac97f2e8a4c1f49ee8ad7f614d88953c
rupak-nm <rupak@neptunemutual.com>
2022-02-22 09:50:19 +0545

    breadcrumb update

--- src/components/UI/organisms/reporting/new/ReportingHero.jsx
+++ src/components/UI/organisms/reporting/new/ReportingHero.jsx
@@ -3,42 +3,51 @@ import { Container } from "@/components/UI/atoms/container";
 import { Hero } from "@/components/UI/molecules/Hero";
 import { CoverProfileInfo } from "@/components/common/CoverProfileInfo";
 import { getCoverImgSrc, getParsedKey } from "@/src/helpers/cover";
+import { useRouter } from "next/router";
 
 export const ReportingHero = ({ coverInfo, reportStatus }) => {
   const imgSrc = getCoverImgSrc(coverInfo);
+  const router = useRouter();
 
   const parsedCoverKey = getParsedKey(coverInfo.key);
+
+  const breadcrumbData = reportStatus
+    ? [
+        { name: "Home", href: "/", current: false },
+        {
+          name: "Reporting",
+          href: `/reporting/${reportStatus.resolved ? "resolved" : "active"}`,
+          current: false,
+        },
+        {
+          name: coverInfo?.coverName,
+          current: !Boolean(reportStatus.dispute),
+          href: reportStatus.dispute
+            ? router.asPath.replace("/dispute", "/details")
+            : "",
+        },
+      ]
+    : [
+        { name: "Home", href: "/", current: false },
+        {
+          name: coverInfo?.coverName,
+          href: `/cover/${parsedCoverKey}/options`,
+          current: false,
+        },
+        { name: "Reporting", current: true },
+      ];
+
+  if (reportStatus?.dispute) {
+    breadcrumbData.push({
+      name: "Dispute",
+      current: true,
+    });
+  }
+
   return (
     <Hero>
       <Container className="px-2 py-20">
-        <BreadCrumbs
-          pages={
-            reportStatus
-              ? [
-                  { name: "Home", href: "/", current: false },
-                  {
-                    name: "Reporting",
-                    href: `/reporting/${
-                      reportStatus.resolved ? "resolved" : "active"
-                    }`,
-                    current: false,
-                  },
-                  {
-                    name: coverInfo?.coverName,
-                    current: true,
-                  },
-                ]
-              : [
-                  { name: "Home", href: "/", current: false },
-                  {
-                    name: coverInfo?.coverName,
-                    href: `/cover/${parsedCoverKey}/options`,
-                    current: false,
-                  },
-                  { name: "Reporting", current: true },
-                ]
-          }
-        />
+        <BreadCrumbs pages={breadcrumbData} />
         <div className="flex">
           <CoverProfileInfo
             imgSrc={imgSrc}
