2da53d794012d001a1c6aab4309e6756fb61bd54
mehta_meet <meet@neptunemutual.com>
2022-02-24 11:53:35 +0530

    added the focus styles

--- src/components/pages/cover/CoverOptionsPage.jsx
+++ src/components/pages/cover/CoverOptionsPage.jsx
@@ -44,7 +44,7 @@ export const CoverOptionsPage = () => {
               >
                 <a
                   className={classNames(
-                    "rounded-2xl md:rounded-3xl group py-6 md:py-12 h-full transition duration-300 ease-out focus:bg-white focus:outline-none",
+                    "rounded-2xl md:rounded-3xl group py-6 md:py-12 h-full transition duration-300 ease-out focus:bg-white focus:outline-none focus-visible:ring-2 focus-visible:ring-4e7dd9",
                     "hover:border-B0C4DB hover:ease-in hover:border-0.5 hover:border-solid  hover:shadow-option  hover:box-border hover:rounded-3xl  hover:bg-white",
                     "border-B0C4DB border-0.5 box-border bg-white lg:bg-transparent lg:border-none"
                   )}
