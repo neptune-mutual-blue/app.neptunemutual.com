a61a9f4ab6e528019e175838cc9b043648a33f2d
rupak-nm <rupak@neptunemutual.com>
2022-02-28 09:43:49 +0545

    alert component updated

--- src/components/UI/atoms/alert/index.jsx
+++ src/components/UI/atoms/alert/index.jsx
@@ -1,12 +1,17 @@
 import ExclamationCircleIcon from "@/icons/ExclamationCircleIcon";
 
-export const Alert = ({ children }) => {
+export const Alert = ({ children, info }) => {
+  const color = {
+    border: info ? "border-4e7dd9" : "border-FA5C2F",
+    icon: info ? "text-4e7dd9" : "text-FA5C2F",
+  };
+
   return (
-    <div className="bg-F4F8FC border border-l-4 border-FA5C2F rounded p-5">
+    <div className={`bg-F4F8FC border border-l-4 ${color.border} rounded p-5`}>
       <div className="flex items-start">
         <div className="flex-shrink-0 pt-0.5">
           <ExclamationCircleIcon
-            className="h-6 w-6 text-FA5C2F"
+            className={`h-6 w-6 ${color.icon}`}
             aria-hidden="true"
           />
         </div>
