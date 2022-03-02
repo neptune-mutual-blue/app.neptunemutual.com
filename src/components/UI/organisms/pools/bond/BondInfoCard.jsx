14929fb2b261ba4b126b1130fc18039e1b218e81
flashburst <82953782+flashburst@users.noreply.github.com>
2022-03-02 17:55:43 +0530

    Remove redundant logo

--- src/components/UI/organisms/pools/bond/BondInfoCard.jsx
+++ src/components/UI/organisms/pools/bond/BondInfoCard.jsx
@@ -1,7 +1,6 @@
 import * as Tooltip from "@radix-ui/react-tooltip";
 
 import { OutlinedCard } from "@/components/UI/molecules/outlined-card";
-import NeptuneMutualCircleLogo from "@/components/UI/atoms/logos/neptune-mutual-circle-logo";
 import InfoCircleIcon from "@/icons/InfoCircleIcon";
 import { BondStatsContainer } from "@/components/UI/molecules/pools/bond/BondStatsContainer";
 import { OutlinedButton } from "@/components/UI/atoms/button/outlined";
@@ -33,7 +32,11 @@ export const BondInfoCard = ({
     <OutlinedCard className="bg-DEEAF6 p-10">
       <div className="flex justify-between items-start">
         <div>
-          <NeptuneMutualCircleLogo width={40} height={40} />
+          <img
+            src="/images/tokens/npm.svg"
+            alt="NPM Logo"
+            className="w-10 h-10"
+          />
           <h3 className="flex items-center text-h4 mt-1 font-sora font-semibold">
             <div>Bond Info</div>
 
