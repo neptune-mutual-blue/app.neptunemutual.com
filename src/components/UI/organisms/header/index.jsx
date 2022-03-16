9b46355bbfbf30a8189aebc73785d281c27d6cb0
naicky-neptune <naicky@neptunemutual.com>
2022-03-16 21:54:30 +0800

    fixed navigation UI on mobile view, close menu modal when clicking menu

--- src/components/UI/organisms/header/index.jsx
+++ src/components/UI/organisms/header/index.jsx
@@ -109,9 +109,9 @@ export const Header = () => {
   const ChainLogo = ChainLogos[networkId] || ChainLogos[1];
 
   const network = (
-    <div className="inline-flex items-center bg-white text-9B9B9B text-sm leading-loose py-2 px-4 border border-transparent rounded-md font-medium overflow-hidden">
+    <div className="inline-flex items-center justify-center w-6/12 px-4 py-2 mr-2 overflow-hidden text-sm font-medium leading-loose bg-white border border-transparent rounded-md md:py-3 lg:py-4 xl:py-2 md:mr-4 xl:w-auto xl:mr-0 text-9B9B9B">
       <ChainLogo width={24} height={24} />{" "}
-      <p className="inline-block ml-2 whitespace-nowrap overflow-hidden text-ellipsis">
+      <p className="inline-block ml-2 overflow-hidden whitespace-nowrap text-ellipsis">
         {NetworkNames[networkId] || "Network"}
       </p>
     </div>
@@ -121,17 +121,17 @@ export const Header = () => {
     <header className="bg-black text-EEEEEE">
       <Banner />
       <nav
-        className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-4 lg:py-0"
+        className="max-w-full px-4 py-4 mx-auto sm:px-6 xl:px-8 xl:py-0"
         aria-label="Top"
       >
-        <div className="w-full flex items-center justify-between lg:border-b border-B0C4DB lg:border-none">
+        <div className="flex items-center justify-between w-full xl:border-b border-B0C4DB xl:border-none">
           <div className="flex items-center">
             <Link href="/">
               <a>
                 <HeaderLogo />
               </a>
             </Link>
-            <div className="ml-16 space-x-8 hidden lg:block">
+            <div className="hidden ml-16 space-x-8 xl:block">
               {navigation.map((link) => {
                 return (
                   <Link key={link.name} href={link.href}>
@@ -152,16 +152,16 @@ export const Header = () => {
           </div>
 
           {!isOpen && (
-            <div className="flex lg:hidden">
+            <div className="flex xl:hidden">
               <BurgerComponent isOpen={isOpen} onToggle={toggleMenu} />
             </div>
           )}
-          <div className="hidden lg:flex">
+          <div className="hidden xl:flex">
             <ConnectWallet networkId={networkId} notifier={notifier}>
               {({ onOpen }) => {
                 let button = (
                   <button
-                    className="inline-block bg-4e7dd9 text-sm leading-loose py-2 px-4 border border-transparent rounded-md font-medium text-white hover:bg-opacity-75"
+                    className="inline-block px-4 py-2 text-sm font-medium leading-loose text-white border border-transparent rounded-md bg-4e7dd9 hover:bg-opacity-75"
                     onClick={onOpen}
                   >
                     Connect Wallet
@@ -170,7 +170,7 @@ export const Header = () => {
                 if (active) {
                   button = (
                     <button
-                      className="relative flex items-center bg-4e7dd9 text-sm leading-loose py-2 px-4 border border-transparent rounded-md font-medium text-white hover:bg-opacity-75"
+                      className="relative flex items-center px-4 py-2 text-sm font-medium leading-loose text-white border border-transparent rounded-md bg-4e7dd9 hover:bg-opacity-75"
                       onClick={handleToggleAccountPopup}
                     >
                       <AccountBalanceWalletIcon width="24" height="24" />
@@ -179,7 +179,7 @@ export const Header = () => {
                   );
                 }
                 return (
-                  <div className="ml-10 space-x-4 py-5 flex border-l border-728FB2 sm:pl-6 lg:pl-8">
+                  <div className="flex py-5 ml-10 space-x-4 border-l border-728FB2 sm:pl-6 xl:pl-8">
                     {network} {button}
                     {isAccountDetailsOpen && (
                       <AccountDetailsModal
@@ -232,6 +232,11 @@ export const MenuModal = ({
 }) => {
   const router = useRouter();
 
+  function handleOnClickNavigate(link) {
+    router.push(link.href);
+    onClose();
+  }
+
   return (
     <div>
       <Dialog.Root open={isOpen} onOpenChange={onClose}>
@@ -246,26 +251,26 @@ export const MenuModal = ({
               &#8203;
             </span>
 
-            <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-y-auto max-h-screen">
-              <div className="max-w-full mx-auto lg:px-8 py-4 lg:py-0 flex justify-end">
+            <Dialog.Content className="fixed max-h-screen min-w-full px-4 overflow-y-auto transform -translate-x-1/2 -translate-y-48 top-48 lg:top-1/4 lg:-translate-y-1/4 left-1/2">
+              <div className="flex justify-end max-w-full py-4 mx-auto mr-4 mb-7 sm:mb-14 xl:px-8 xl:py-0">
                 <BurgerComponent isOpen={isOpen} onToggle={onClose} />
               </div>
-              <div className="inline-block w-full max-w-md p-6 my-8 text-left align-middle sm:align-baseline transition-all transform shadow-xl rounded-2xl">
+              <div className="inline-block w-full px-6 text-left align-middle transition-all transform shadow-xl sm:px-20 sm:align-baseline rounded-2xl">
                 <div className="flex flex-col max-h-[70vh] overflow-y-auto justify-start">
                   {navigation.map((link) => {
                     return (
-                      <Link key={link.name} href={link.href}>
-                        <a
-                          className={classNames(
-                            "text-h2 leading-6 sm:text-xxl pt-8 sm:pt-12 pb-3 sm:pb-4 mb-5 sm:mb-8 border-b-4 w-fit",
-                            router.pathname == link.href
-                              ? "border-4e7dd9 text-4e7dd9 font-semibold"
-                              : "border-transparent text-white"
-                          )}
-                        >
-                          {link.name}
-                        </a>
-                      </Link>
+                      <a
+                        className={classNames(
+                          "text-h2 leading-6 sm:text-xxl pt-8 sm:pt-12 pb-3 sm:pb-4 mb-5 sm:mb-8 border-b-4 w-fit",
+                          router.pathname == link.href
+                            ? "border-4e7dd9 text-4e7dd9 font-semibold"
+                            : "border-transparent text-white"
+                        )}
+                        key={link.name}
+                        onClick={() => handleOnClickNavigate(link)}
+                      >
+                        {link.name}
+                      </a>
                     );
                   })}
                 </div>
@@ -274,7 +279,7 @@ export const MenuModal = ({
                     {({ onOpen }) => {
                       let button = (
                         <button
-                          className="inline-block bg-4e7dd9 text-sm leading-loose py-2 px-4 border border-transparent rounded-md font-medium text-white hover:bg-opacity-75"
+                          className="justify-center inline-block w-6/12 px-4 py-2 ml-2 text-sm font-medium leading-none text-white border border-transparent rounded-md md:py-3 lg:py-4 xl:py-2 md:ml-4 bg-4e7dd9 hover:bg-opacity-75"
                           onClick={onOpen}
                         >
                           Connect Wallet
@@ -283,7 +288,7 @@ export const MenuModal = ({
                       if (active) {
                         button = (
                           <button
-                            className="relative flex items-center bg-4e7dd9 text-sm leading-loose py-2 px-4 border border-transparent rounded-md font-medium text-white hover:bg-opacity-75"
+                            className="relative flex items-center justify-center w-6/12 px-4 py-2 ml-2 text-sm font-medium leading-loose text-white border border-transparent rounded-md md:py-3 lg:py-4 xl:py-2 md:ml-4 bg-4e7dd9 hover:bg-opacity-75"
                             onClick={handleToggleAccountPopup}
                           >
                             <AccountBalanceWalletIcon width="24" height="24" />
@@ -294,7 +299,7 @@ export const MenuModal = ({
                         );
                       }
                       return (
-                        <div className="py-5 flex justify-between sm:pl-6 lg:pl-8">
+                        <div className="flex justify-between py-5">
                           {network} {button}
                           {isAccountDetailsOpen && (
                             <AccountDetailsModal
