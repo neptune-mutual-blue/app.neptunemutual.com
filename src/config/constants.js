63ffe1d07c397e585f4d9c04905e076c299f6423
flashburst <82953782+flashburst@users.noreply.github.com>
2022-03-22 06:26:14 +0530

    Make API server configurable

--- src/config/constants.js
+++ src/config/constants.js
@@ -13,12 +13,16 @@ export const CoverStatus = {
   4: "Claimable",
 };
 
-// Should be added to CSP
-export const PRICING_URL = "https://api.neptunemutual.com/pricing/{networkId}";
+// Will end with `/`
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
+  ? new URL(process.env.NEXT_PUBLIC_API_URL).toString()
+  : "/";
+
+export const PRICING_URL = `${API_BASE_URL}pricing/{networkId}`;
+export const UNSTAKE_INFO_URL = `${API_BASE_URL}protocol/consensus/unstake-info/{networkId}/{coverKey}/{account}/{incidentDate}`;
+
 export const FAUCET_URL = "https://faucet.neptunemutual.com/";
 export const LEADERBOARD_URL = "https://leaderboard.neptunemutual.com/";
-export const UNSTAKE_INFO_URL =
-  "https://api.neptunemutual.com/protocol/consensus/unstake-info/{networkId}/{coverKey}/{account}/{incidentDate}";
 
 export const POOL_URLS = {
   3: "https://app.sushi.com/add/{liquidityTokenAddress}/{NPMTokenAddress}",
