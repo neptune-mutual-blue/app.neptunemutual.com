5807a891c3f23781160d67bfb8d916ce22043b6d
rupak-nm <rupak@neptunemutual.com>
2022-03-23 09:35:13 +0545

    icons updated

--- lib/toast/components/icons/InfoIcon.jsx
+++ lib/toast/components/icons/InfoIcon.jsx
@@ -7,7 +7,7 @@ const InfoIcon = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <circle cx={12} cy={12} r={11} fill="#FA5C2F" />
+    <circle cx={12} cy={12} r={11} fill="currentColor" />
     <path
       d="M13 7.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-3 3.75a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 .75.75v4.25h.75a.75.75 0 1 1 0 1.5h-3a.75.75 0 1 1 0-1.5h.75V12h-.75a.75.75 0 0 1-.75-.75Z"
       fill="#fff"
