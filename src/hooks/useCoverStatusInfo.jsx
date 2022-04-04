80dbf844a7619069aea8bd1d016ba74108a8dfe8
savinnpm <savin@neptunemutual.com>
2022-04-04 10:57:37 +0545

    changed 'whitelisted' from string to bool

--- src/hooks/useCoverStatusInfo.jsx
+++ src/hooks/useCoverStatusInfo.jsx
@@ -8,7 +8,7 @@ export const useCoverStatusInfo = (coverKey) => {
   const [statusInfo, setStatusInfo] = useState({
     activeIncidentDate: "0",
     status: "",
-    whitelisted: "",
+    whitelisted: false,
     requiresWhitelist: false,
   });
   const { account, library } = useWeb3React();
