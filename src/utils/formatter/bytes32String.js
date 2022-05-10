84e8412486448aab55809ca9b5e553b6db5f1cf9
savinnpm <savin@neptunemutual.com>
2022-05-10 14:15:31 +0545

    added file in formatter

---
+++ src/utils/formatter/bytes32String.js
@@ -0,0 +1,20 @@
+import {
+  parseBytes32String,
+  formatBytes32String,
+} from "@ethersproject/strings";
+
+export const safeParseBytes32String = (bytes32String) => {
+  try {
+    return parseBytes32String(bytes32String);
+  } catch (error) {
+    return bytes32String;
+  }
+};
+
+export const safeFormatBytes32String = (str) => {
+  try {
+    return formatBytes32String(str);
+  } catch (error) {
+    return str;
+  }
+};
