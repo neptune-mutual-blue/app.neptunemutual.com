f2e8a806c3af122841f5787caba66240700fe2da
Marlo Dela Torre <mato@splitmedialabs.com>
2022-05-18 15:04:26 +0800

    code cleanup

--- src/modules/pools/staking/__tests__/staking.test.jsx
+++ src/modules/pools/staking/__tests__/staking.test.jsx
@@ -1,196 +1,105 @@
-import {
-  fireEvent,
-  waitFor,
-  withProviders,
-  withSorting,
-} from "@/utils/unit-tests/test-utils";
+import { withProviders } from "@/utils/unit-tests/test-utils";
 import { act } from "react-dom/test-utils";
-
+import { contracts, getCover, pricing, QUERY_RESULT } from "./mockUpdata.data";
+import { API_BASE_URL } from "@/src/config/constants";
 import { StakingPage } from "@/modules/pools/staking";
 import { i18n } from "@lingui/core";
 import ReactDOM from "react-dom";
 
-describe("Pool Staking", () => {
-  const Component = withProviders(withSorting(StakingPage));
-  const container = document.createElement("div");
-  beforeAll(async () => {
-    act(() => {
-      i18n.activate("en");
-      ReactDOM.render(<Component />, container);
-    });
-    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(11));
+const MOCKUP_API_URLS = {
+  POOL_INFO_URL: `${API_BASE_URL}protocol/staking-pools/info/`,
+  GET_CONTRACTS_INFO_URL: `${process.env.NEXT_PUBLIC_API_URL}/protocol/contracts/`,
+  SUB_GRAPH: process.env.NEXT_PUBLIC_MUMBAI_SUBGRAPH_URL,
+};
+
+const QUERY = {
+  PLATFORM_FEE: "platformFee",
+  IPS_HASH: "ipfsHash",
+  TOTAL_ADDED_TO_BOND: "totalLpAddedToBond",
+};
+
+async function mockFetch(url, { body }) {
+  if (url.startsWith(MOCKUP_API_URLS.POOL_INFO_URL)) {
+    return {
+      ok: true,
+      status: 200,
+      json: async () => getCover(url),
+    };
+  }
+
+  if (url.startsWith(MOCKUP_API_URLS.GET_CONTRACTS_INFO_URL)) {
+    // console.log("return contracts");
+    return {
+      ok: true,
+      status: 200,
+      json: async () => contracts,
+    };
+  }
+
+  if (url.startsWith(MOCKUP_API_URLS.FINANCING)) {
+    // console.log("return financing");
+    return {
+      ok: true,
+      status: 200,
+      json: async () => pricing,
+    };
+  }
+
+  if (url.startsWith(MOCKUP_API_URLS.SUB_GRAPH)) {
+    if (body.includes(QUERY.PLATFORM_FEE)) {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => QUERY_RESULT.PLATFORM_FEE,
+      };
+    }
+
+    if (body.includes(QUERY.IPS_HASH)) {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => QUERY_RESULT.IPFSHASH,
+      };
+    }
+
+    if (body.includes(QUERY.TOTAL_ADDED_TO_BOND)) {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => QUERY_RESULT.TOTAL_ADDED_TO_BOND,
+      };
+    }
+  }
+
+  throw new Error(`Unhandled request: ${url}`);
+}
+
+global.fetch = jest.fn(mockFetch);
+global.ethereum = {
+  enable: jest.fn(() => {}),
+  eth_requestAccounts: () => {},
+};
+
+function delay(ms = 1000) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
   });
+}
 
-  describe("Staking card", () => {
-    it("Card should be 6", () => {
-      const stakeCards = select(container, SELECTION.TITLE);
+describe("Pool Staking", () => {
+  beforeAll(() => act(() => i18n.activate("en")));
+  const Component = withProviders(StakingPage);
 
-      expect(stakeCards.length).toEqual(6);
+  it("Staking card should be 6", async () => {
+    const container = document.createElement("div");
+    await act(() => {
+      ReactDOM.render(<Component />, container);
+      return delay(1000);
     });
+    const stakeCards = container.querySelectorAll("h4");
 
-    describe("TVL", () => {
-      it("should be 6", () => {
-        const tvlElements = select(container, SELECTION.TVL);
-        expect(tvlElements.length).toEqual(6);
-      });
-
-      it("no 0 value", () => {
-        const tvlValues = getValues(container, SELECTION.TVL);
-
-        const zeroValues = tvlValues.filter((value) => value === 0);
-
-        expect(zeroValues.length).toEqual(0);
-      });
-    });
-
-    describe("APR", () => {
-      it("should be 6", () => {
-        const aprElements = select(container, SELECTION.APR);
-        expect(aprElements.length).toEqual(6);
-      });
-
-      it("no 0 value", () => {
-        const aprValues = getValues(container, SELECTION.APR);
-        const zeroValues = aprValues.filter((value) => value === 0);
-
-        expect(zeroValues.length).toEqual(0);
-      });
-    });
-
-    describe("Sorting", () => {
-      it("Sorting is visible", () => {
-        const sortButton = container.querySelector("button");
-
-        act(() => {
-          fireEvent.click(sortButton);
-        });
-
-        const sortList = container.querySelector(
-          `[aria-labelledby='${sortButton.id}']`
-        );
-
-        expect(container).toContainElement(sortList);
-      });
-
-      it("Sort Alphabetically", () => {
-        const original = getValues(container, SELECTION.TITLE);
-
-        const sortButton = container.querySelector("button");
-
-        act(() => {
-          fireEvent.select(sortButton);
-        });
-
-        const sortList = container.querySelector(
-          `[aria-labelledby='${sortButton.id}']`
-        );
-
-        const [alphabet] = Array.from(sortList.querySelectorAll("li"));
-
-        act(() => {
-          fireEvent.click(alphabet);
-        });
-
-        const values = getValues(container, SELECTION.TITLE);
-
-        expect(original).toEqual(values);
-      });
-
-      it("Sort by TVL", () => {
-        const original = getValues(container, SELECTION.TVL);
-
-        const sortButton = container.querySelector("button");
-
-        act(() => {
-          fireEvent.click(sortButton);
-        });
-
-        const sortList = container.querySelector(
-          `[aria-labelledby='${sortButton.id}']`
-        );
-
-        const [_, tvl] = Array.from(sortList.querySelectorAll("li"));
-
-        act(() => {
-          fireEvent.click(tvl);
-        });
-
-        const values = getValues(container, SELECTION.TVL);
-        const sortedOriginal = [...original].sort(sortFromHighest);
-
-        expect(original).not.toEqual(values);
-        expect(sortedOriginal).toEqual(values);
-      });
-
-      it("Sort by APR", () => {
-        const original = getValues(container, SELECTION.APR);
-
-        const sortButton = container.querySelector("button");
-
-        act(() => {
-          fireEvent.click(sortButton);
-        });
-
-        const sortList = container.querySelector(
-          `[aria-labelledby='${sortButton.id}']`
-        );
-
-        const [_, _tvl, apr] = Array.from(sortList.querySelectorAll("li"));
-
-        act(() => {
-          fireEvent.click(apr);
-        });
-
-        const values = getValues(container, SELECTION.APR);
-        const sortedOriginal = [...original].sort(sortFromHighest);
-
-        expect(original).not.toEqual(values);
-        expect(sortedOriginal).toEqual(values);
-      });
-    });
+    expect(stakeCards.length).toEqual(6);
   });
 });
-
-const SELECTION = {
-  TITLE: "title",
-  TVL: "tvl",
-  APR: "apr",
-};
-
-const select = (container, type) => {
-  if (type === SELECTION.TITLE) {
-    return Array.from(container.querySelectorAll("h4"));
-  }
-
-  if (type === SELECTION.TVL) {
-    return Array.from(
-      container.querySelectorAll(".text-right[title] p.text-7398C0")
-    );
-  }
-
-  return Array.from(container.querySelectorAll(".text-21AD8C"));
-};
-
-const getValues = (container, type) => {
-  if (type === SELECTION.TITLE) {
-    return select(container, type).map((el) => el.textContent);
-  }
-
-  if (type === SELECTION.TVL) {
-    return select(container, type).map((el) =>
-      parseFloat(el.textContent.slice(1), 10)
-    );
-  }
-
-  return select(container, type).map((el) =>
-    parseFloat(el.textContent.slice("APR: ".length), 10)
-  );
-};
-
-const sortFromHighest = (a, b) => {
-  if (a === b) {
-    return 0;
-  }
-
-  return a > b ? -1 : 1;
-};
