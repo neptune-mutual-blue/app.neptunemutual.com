9e9b79e7f3fae005e965cb752f4f02c628e49379
savinnpm <savin@neptunemutual.com>
2022-05-23 09:40:56 +0545

    added tests for common components

---
+++ src/common/Alert/__tests__/Alert.test.jsx
@@ -0,0 +1,25 @@
+import { screen, act, render } from "@/utils/unit-tests/test-utils";
+import { Alert } from "@/common/Alert/Alert";
+import { i18n } from "@lingui/core";
+
+describe("alert component behaviour", () => {
+  beforeAll(() => {
+    act(() => {
+      i18n.activate("en");
+    });
+  });
+
+  it("should render any children passed to it", () => {
+    render(<Alert>This shows alert text.</Alert>);
+    const divElement = screen.getByText(/This shows alert text./i);
+    expect(divElement).toBeInTheDocument();
+  });
+
+  it("should have class if info prop is passed to it", async () => {
+    const screen = render(
+      <Alert info>This shows text with blue border.</Alert>
+    );
+    const results = screen.container.getElementsByClassName("border-4e7dd9");
+    expect(results.length).toEqual(1);
+  });
+});
