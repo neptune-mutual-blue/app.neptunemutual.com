d61f41d8b7319b9147438e92aaf852a3bad32ab0
savinnpm <savin@neptunemutual.com>
2022-05-25 11:54:41 +0545

    added tests for common components

---
+++ src/common/BurgerMenu/__tests__/BurgerMenu.test.jsx
@@ -0,0 +1,21 @@
+import { BurgerMenu } from "@/common/BurgerMenu/BurgerMenu";
+import { act, render } from "@/utils/unit-tests/test-utils";
+import { i18n } from "@lingui/core";
+
+describe("BurgerMenu component", () => {
+  beforeAll(() => {
+    act(() => {
+      i18n.activate("en");
+    });
+  });
+
+  const isOpen = true;
+
+  test("should have class 'z-20' when opened", () => {
+    const { container } = render(
+      <BurgerMenu isOpen={isOpen} onToggle={() => {}} />
+    );
+    let classes = container.getElementsByClassName("z-20");
+    expect(classes.length).toEqual(1);
+  });
+});
---
+++ src/common/Cover/__tests__/CoverActionCard.test.jsx
@@ -0,0 +1,55 @@
+import { CoverActionCard } from "@/common/Cover/CoverActionCard";
+import { screen, act, render } from "@/utils/unit-tests/test-utils";
+import { i18n } from "@lingui/core";
+
+describe("CoverActionCard component", () => {
+  beforeAll(() => {
+    act(() => {
+      i18n.activate("en");
+    });
+  });
+
+  const mockProps = {
+    title: "Provide Liquidity",
+    description: "to pool risks and receive rewards",
+    imgSrc: "/cover-actions/add-liquidity.svg",
+  };
+
+  test("should render title passed to it", () => {
+    render(
+      <CoverActionCard
+        title={mockProps.title}
+        description={mockProps.description}
+        imgSrc={mockProps.imgSrc}
+      />
+    );
+    const titleElement = screen.getByRole("heading", { level: 4 });
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toHaveTextContent(mockProps.title);
+  });
+
+  test("should render description passed to it", () => {
+    render(
+      <CoverActionCard
+        title={mockProps.title}
+        description={mockProps.description}
+        imgSrc={mockProps.imgSrc}
+      />
+    );
+    const description = screen.getByText(/pool risks and receive rewards/i);
+    expect(description).toBeInTheDocument();
+  });
+
+  test("should render img passed to it", () => {
+    const screen = render(
+      <CoverActionCard
+        title={mockProps.title}
+        description={mockProps.description}
+        imgSrc={mockProps.imgSrc}
+      />
+    );
+    const imageElement = screen.getByRole("img");
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute("src", mockProps.imgSrc);
+  });
+});
