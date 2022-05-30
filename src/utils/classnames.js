e866342578a4a4fbad14640210eb003565aa53f0
Marlo Dela Torre <mato@splitmedialabs.com>
2022-05-30 12:53:49 +0800

    unit testing for utils

--- src/utils/classnames.js
+++ src/utils/classnames.js
@@ -4,5 +4,31 @@
  * @returns {string} string of concatenated classes
  */
 export function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  const finalClasses = [];
+
+  for (const value of classes) {
+    if (typeof value === "string") {
+      const trimedValue = value.trim();
+      if (trimedValue.length) {
+        finalClasses.push(value);
+      }
+      continue;
+    }
+
+    if (typeof value === "object" && !Array.isArray(value)) {
+      const arrayOfClasses = Object.entries(value).reduce(
+        (arr, [key, value]) => {
+          if (value) {
+            arr.push(key);
+          }
+
+          return arr;
+        }, []
+      );
+
+      finalClasses.push(...arrayOfClasses);
+    }
+  }
+
+  return finalClasses.join(" ");
 }
