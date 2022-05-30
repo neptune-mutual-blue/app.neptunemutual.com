889890458b34a47caa034fc962c81801f3e9b20d
Marlo Dela Torre <mato@splitmedialabs.com>
2022-05-31 04:38:19 +0800

    utils testing

---
+++ src/services/contracts/__tests__/getAddresses.test.js
@@ -0,0 +1,29 @@
+const {
+  getAddressesFromApi,
+} = require("@/src/services/contracts/getAddresses");
+const { contracts } = require("@/utils/unit-tests/data/mockUpdata.data");
+const { mockFetch } = require("@/utils/unit-tests/mockApiRequest");
+
+const { value: NPMTokenAddress } = contracts.data.find(
+  (item) => item.key === "NPM"
+);
+const { value: liquidityTokenAddress } = contracts.data.find(
+  (item) => item.key === "Stablecoin"
+);
+
+describe("Get Address", () => {
+  global.fetch = jest.fn(mockFetch);
+
+  test("get address", async () => {
+    const result = await getAddressesFromApi(
+      process.env.NEXT_PUBLIC_FALLBACK_NETWORK
+    );
+
+    const expected = {
+      NPMTokenAddress,
+      liquidityTokenAddress,
+    };
+
+    expect(result).toStrictEqual(expected);
+  });
+});
