f55e2437dd361bc7debc30a619d7e131195e94ef
savinnpm <savin@neptunemutual.com>
2022-05-31 10:07:19 +0545

    added test for modal

---
+++ src/common/Modal/__tests__/ModalWrapper.test.jsx
@@ -0,0 +1,17 @@
+import { ModalWrapper } from "@/common/Modal/ModalWrapper";
+import { render, act } from "@/utils/unit-tests/test-utils";
+import { i18n } from "@lingui/core";
+
+describe("ModalWrapper behaviour", () => {
+  beforeAll(() => {
+    act(() => {
+      i18n.activate("en");
+    });
+  });
+
+  test("should render any children passed to it", () => {
+    const screen = render(<ModalWrapper>Modal Title</ModalWrapper>);
+    const textElement = screen.getByText(/Modal Title/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
