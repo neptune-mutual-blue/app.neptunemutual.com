db416cb759798efb054992fb6bcae304c91990f7
savinnpm <savin@neptunemutual.com>
2022-06-01 11:57:44 +0545

    added condition to check the image is available or not

--- src/hooks/useRegisterToken.jsx
+++ src/hooks/useRegisterToken.jsx
@@ -2,16 +2,25 @@ import { registerToken } from "@/lib/connect-wallet/utils/wallet";
 import { API_BASE_URL } from "@/src/config/constants";
 import { useNetwork } from "@/src/context/Network";
 import { useWeb3React } from "@web3-react/core";
+import { useState } from "react";
 
 export const useRegisterToken = () => {
   const { networkId } = useNetwork();
   const { account } = useWeb3React();
+  const [isImageAvailable, setIsImageAvailable] = useState(false);
 
-  const register = (address, symbol) => {
+  const register = async (address, symbol) => {
     if (!networkId || !account) return;
-
-    const image = symbol
-      ? `${API_BASE_URL}images/tokens/${symbol.toLowerCase()}.svg`
+    symbol &&
+      (await fetch(
+        `${API_BASE_URL}/images/tokens/${symbol.toLowerCase()}.svg`
+      ).then((res) => {
+        if (res.status != "404") {
+          setIsImageAvailable(true);
+        }
+      }));
+    const image = isImageAvailable
+      ? `${API_BASE_URL}/images/tokens/${symbol.toLowerCase()}.svg`
       : undefined;
 
     registerToken(address, symbol, 18, image)
