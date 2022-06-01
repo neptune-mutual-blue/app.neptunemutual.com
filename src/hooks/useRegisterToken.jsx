9d0aa41835da3ff3c4640ff7e1e4baa2511fd9dc
savinnpm <savin@neptunemutual.com>
2022-06-01 12:34:56 +0545

    refactored to remove useState

--- src/hooks/useRegisterToken.jsx
+++ src/hooks/useRegisterToken.jsx
@@ -2,27 +2,26 @@ import { registerToken } from "@/lib/connect-wallet/utils/wallet";
 import { API_BASE_URL } from "@/src/config/constants";
 import { useNetwork } from "@/src/context/Network";
 import { useWeb3React } from "@web3-react/core";
-import { useState } from "react";
 
 export const useRegisterToken = () => {
   const { networkId } = useNetwork();
   const { account } = useWeb3React();
-  const [isImageAvailable, setIsImageAvailable] = useState(false);
+
+  const fetchImage = async (symbol) => {
+    let image = undefined;
+    if (symbol) {
+      const res = await fetch(
+        `${API_BASE_URL}images/tokens/${symbol.toLowerCase()}.svg`
+      );
+      if (res.status != 404)
+        image = `${API_BASE_URL}/images/tokens/${symbol.toLowerCase()}.svg`;
+    }
+    return image;
+  };
 
   const register = async (address, symbol) => {
     if (!networkId || !account) return;
-    symbol &&
-      (await fetch(
-        `${API_BASE_URL}images/tokens/${symbol.toLowerCase()}.svg`
-      ).then((res) => {
-        if (res.status != "404") {
-          setIsImageAvailable(true);
-        }
-      }));
-    const image = isImageAvailable
-      ? `${API_BASE_URL}/images/tokens/${symbol.toLowerCase()}.svg`
-      : undefined;
-
+    const image = await fetchImage(symbol);
     registerToken(address, symbol, 18, image)
       .then(console.log)
       .catch(console.error);
