de5d808f825398fc7f6bbfbfc49546a7e0d8b3f3
savinnpm <savin@neptunemutual.com>
2022-06-15 15:54:13 +0545

    added props to left arrow

--- src/icons/LeftArrow.jsx
+++ src/icons/LeftArrow.jsx
@@ -6,8 +6,10 @@ const LeftArrow = (props) => (
     height={8}
     xmlns="http://www.w3.org/2000/svg"
     className={classNames(
-      props.variant === "right" ? "ml-2 rotate-180" : "mr-2"
+      props.variant === "right" ? "ml-2 rotate-180" : "mr-2",
+      props.className
     )}
+    {...props}
   >
     <path
       d="M.646 3.646a.5.5 0 0 0 0 .708l3.182 3.182a.5.5 0 1 0 .708-.708L1.707 4l2.829-2.828a.5.5 0 1 0-.708-.708L.646 3.646ZM1 4.5h23v-1H1v1Z"
