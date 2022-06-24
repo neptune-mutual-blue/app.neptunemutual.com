8562332bf96ff4c93503d7012433e5731afb1649
al-neptune <al@neptunemutual.com>
2022-06-24 10:06:49 +0800

    fix: font color overriding

--- src/common/Badge/Badge.jsx
+++ src/common/Badge/Badge.jsx
@@ -2,14 +2,16 @@ import { classNames } from "@/utils/classnames";
 
 export const Badge = ({ children, className }) => {
   return (
-    <div
-      className={classNames(
-        "inline-block px-2 border rounded-xl",
-        "font-poppins text-xs whitespace-nowrap",
-        className
-      )}
-    >
-      {children}
+    <div className="text-FEFEFF">
+      <div
+        className={classNames(
+          "inline-block px-2 border rounded",
+          "font-poppins text-xs whitespace-nowrap",
+          className
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
