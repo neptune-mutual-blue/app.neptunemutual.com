6e5a5d04af0dfa78708d24e3268113ab1840e43e
al-neptune <al@neptunemutual.com>
2022-07-19 11:56:25 +0800

    fix: overflow on mobile navigation modal

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -309,7 +309,7 @@ export const MenuModal = ({
         <Portal>
           <Overlay className="fixed inset-0 z-40 overflow-y-auto bg-black bg-opacity-80 backdrop-blur-xl" />
 
-          <Content className="fixed z-50 max-h-screen min-w-full px-4 overflow-y-auto transform -translate-x-1/2 -translate-y-48 top-48 lg:top-1/4 lg:-translate-y-1/4 left-1/2">
+          <Content className="fixed z-50 max-h-screen w-full px-4 overflow-y-auto transform -translate-x-1/2 -translate-y-48 top-48 lg:top-1/4 lg:-translate-y-1/4 left-1/2">
             <div className="flex flex-col items-end justify-between min-h-screen px-4 text-center">
               <div className="flex justify-end w-full max-w-full pt-6 mx-auto mb-7 sm:mb-14 xl:px-8 xl:py-0">
                 <BurgerMenu isOpen={isOpen} onToggle={onClose} />
