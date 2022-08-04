e661c934c220b0126c7c7affbd12ef6c46f85751
savinnpm <savin@neptunemutual.com>
2022-08-04 14:14:10 +0545

    fixed card badge

--- src/common/CardStatusBadge.jsx
+++ src/common/CardStatusBadge.jsx
@@ -15,9 +15,9 @@ export const CardStatusBadge = ({ status }) => {
       className={classNames(
         "!rounded",
         isRed && "bg-FA5C2F",
-        isGray && "bg-364253",
         isBlue && "bg-4289F2",
-        !isGray && !isRed && "bg-21AD8C"
+        isGray && "bg-364253",
+        !isGray && !isRed && !isBlue && "bg-21AD8C"
       )}
       data-testid="card-badge"
     >
