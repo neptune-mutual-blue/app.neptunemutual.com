777e74ea0d1481dfde5d8f047b8122139507e542
savinnpm <savin@neptunemutual.com>
2022-08-04 13:19:20 +0545

    change claimable status color to be consistent across app

--- src/common/CardStatusBadge.jsx
+++ src/common/CardStatusBadge.jsx
@@ -2,7 +2,8 @@ import { Badge } from "@/common/Badge/Badge";
 import { classNames } from "@/utils/classnames";
 
 export const CardStatusBadge = ({ status }) => {
-  const isRed = ["Incident Happened", "Claimable"].includes(status);
+  const isRed = ["Incident Happened"].includes(status);
+  const isBlue = ["Claimable"].includes(status);
   const isGray = ["Diversified"].includes(status);
 
   if (!status || status == "Normal") {
@@ -15,6 +16,7 @@ export const CardStatusBadge = ({ status }) => {
         "!rounded",
         isRed && "bg-FA5C2F",
         isGray && "bg-364253",
+        isBlue && "bg-4289F2",
         !isGray && !isRed && "bg-21AD8C"
       )}
       data-testid="card-badge"
