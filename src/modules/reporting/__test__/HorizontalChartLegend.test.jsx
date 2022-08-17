b42580ad864a3c926fb8e3f75783c031a7e3780c
savinnpm <savin@neptunemutual.com>
2022-08-17 16:29:01 +0545

    added test to reporting modules

---
+++ src/modules/reporting/__test__/HorizontalChartLegend.test.jsx
@@ -0,0 +1,19 @@
+import { i18n } from "@lingui/core";
+import { render, screen } from "@/utils/unit-tests/test-utils";
+import { mockFn } from "@/utils/unit-tests/test-mockup-fn";
+import { HorizontalChartLegend } from "@/modules/reporting/HorizontalChartLegend";
+
+describe("HorizontalChartLegend test", () => {
+  beforeEach(() => {
+    i18n.activate("en");
+
+    mockFn.useAppConstants();
+    mockFn.useCoverOrProductData();
+  });
+
+  test("should render the Incident Occurred legend", () => {
+    render(<HorizontalChartLegend />);
+    const wrapper = screen.getByText(/Incident Occurred/i);
+    expect(wrapper).toBeInTheDocument();
+  });
+});
