5344db42adfc80e526b75743bd9886cfb0438b83
savinnpm <savin@neptunemutual.com>
2022-08-22 09:03:31 +0545

    added test for recent votes table

---
+++ src/modules/reporting/__test__/RecentVotesTable.test.jsx
@@ -0,0 +1,33 @@
+import { i18n } from "@lingui/core";
+import { render } from "@/utils/unit-tests/test-utils";
+import { testData } from "@/utils/unit-tests/test-data";
+import { RecentVotesTable } from "@/modules/reporting/RecentVotesTable";
+import { mockFn } from "@/utils/unit-tests/test-mockup-fn";
+
+jest.mock("react-chartjs-2", () => ({
+  Doughnut: (p) => mockFn.chartMockFn(p),
+  Bar: (p) => mockFn.chartMockFn(p),
+}));
+
+describe("RecentVotesTable test", () => {
+  beforeEach(() => {
+    i18n.activate("en");
+    mockFn.useRecentVotes();
+  });
+
+  const incidentReport = testData.incidentReports.data.incidentReport;
+
+  test("should render the recent votes table", () => {
+    const screen = render(
+      <RecentVotesTable
+        coverKey={incidentReport.coverKey}
+        productKey={incidentReport.productKey}
+        incidentDate={incidentReport.incidentDate}
+      />
+    );
+    const wrapper = screen.getByRole("table");
+    const tableHeading = screen.getByText("Recent Votes");
+    expect(wrapper).toBeInTheDocument();
+    expect(tableHeading).toBeInTheDocument();
+  });
+});
