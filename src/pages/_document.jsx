6e414996d2511bff962ce9f8cf7d9cbca3f258ee
al-neptune <al@neptunemutual.com>
2022-08-26 09:04:12 +0800

    feat: install tag manager in document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <Script
+            id="my-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WKSTFFB');`,
+            }}
+          ></Script>
           <link
             rel="apple-touch-icon"
             sizes="57x57"
@@ -86,6 +98,12 @@ class MyDocument extends Document {
           translate="no"
           className="text-black font-poppins text-para bg-f1f3f6"
         >
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKSTFFB"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
