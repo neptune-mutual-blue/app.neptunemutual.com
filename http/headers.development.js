233003929a8afe6e587f4d3a1c9249b615cbdccd
flashburst <82953782+flashburst@users.noreply.github.com>
2022-08-30 16:51:52 +0530

    Fix button appearance on safari

--- http/headers.development.js
+++ http/headers.development.js
@@ -3,5 +3,7 @@ const development = JSON.parse(JSON.stringify(production));
 
 // script-src
 development[0].values[0] = `${development[0].values[0]} 'unsafe-eval' 'unsafe-inline'`;
+development[0].values[1] = `${development[0].values[1]} http://localhost:3000 ws://localhost:3000`;
+development[0].values[3] = ``;
 
 module.exports = development;
