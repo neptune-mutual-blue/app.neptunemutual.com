37f1586d0c8e4bde7d60c06686326d19d82e85e4
al-neptune <al@neptunemutual.com>
2022-09-02 12:19:16 +0800

    feat: add correct value to gtm

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const http = require("./http");
 module.exports = () => {
   return {
     env: {
-      GTM_ID: "my-value",
+      GTM_ID: "GTM-WKSTFFB",
     },
     reactStrictMode: true,
     experimental: {
