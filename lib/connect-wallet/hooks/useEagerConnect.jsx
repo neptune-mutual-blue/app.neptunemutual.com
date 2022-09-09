e4eed91d330cdff8d8b23126f8ab3fe6ea7ef080
Marlo Dela Torre <mato@splitmedialabs.com>
2022-09-09 14:22:35 +0800

    custom wallet library test cases with 100% coverage

--- lib/connect-wallet/hooks/useEagerConnect.jsx
+++ lib/connect-wallet/hooks/useEagerConnect.jsx
@@ -22,7 +22,6 @@ export const useEagerConnect = (networkId, notifier) => {
 
   useEffect(() => {
     const connectorName = window.localStorage.getItem(ACTIVE_CONNECTOR_KEY);
-
     if (!connectorName) {
       console.info("Unable to find connector from local storage");
       return;
