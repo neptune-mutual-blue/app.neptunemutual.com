1bf8664adc7b0d816acc49b069fc3e72b7051ad4
savinnpm <savin@neptunemutual.com>
2022-09-09 10:23:24 +0545

    renaming test

--- src/services/covers-products/__tests__/index.test.jsx
+++ src/services/covers-products/__tests__/index.test.jsx
@@ -1,15 +1,16 @@
 import { getNetworkId } from "@/src/config/environment";
 import { getCoverData } from "@/src/services/covers-products";
+import { mockFetch } from "@/utils/unit-tests/mockApiRequest";
 
 describe("getCoverData function test", () => {
-  test("should return data", async () => {
+  test("should return null", async () => {
+    global.fetch = jest.fn(mockFetch);
+
     let coverdata = await getCoverData(
       getNetworkId(),
       0x6262382d65786368616e67650000000000000000000000000000000000000000
     );
-
     console.log(coverdata);
-
     expect(coverdata).toBe(null);
   });
 });
