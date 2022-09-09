a7519fb5316da718dd6a663e1cd90e1778503ad8
Marlo Dela Torre <mato@splitmedialabs.com>
2022-09-09 08:35:43 +0800

    connect wallet test cases

---
+++ lib/connect-wallet/components/ConnectWallet/__tests__/CustomWallet.test.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { fireEvent, screen } from "@/utils/unit-tests/test-utils";
+import { initiateTest, mockFn } from "@/utils/unit-tests/test-mockup-fn";
+import { i18n } from "@lingui/core";
+import { Popup } from "./../Popup";
+import { testData } from "@/utils/unit-tests/test-data";
+jest.mock("../../../config/wallets", () => ({
+  wallets: [
+    {
+      id: "3",
+      name: "Custom Wallet",
+      connectorName: "injected",
+      Icon: () => <></>,
+    },
+  ],
+}));
+
+describe("Popup Component", () => {
+  const onLogin = jest.fn(() => {});
+  const onLogout = jest.fn(() => {});
+  const onClose = jest.fn(() => {});
+  const notifier = jest.fn(() => {});
+  const { initialRender, rerenderFn } = initiateTest(Popup, {
+    isOpen: true,
+    onClose: onClose,
+    networkId: testData.network.networkId,
+    notifier: notifier,
+  });
+
+  beforeEach(() => {
+    mockFn.useAuth(() => ({
+      login: onLogin,
+      logout: onLogout,
+    }));
+    i18n.activate("en");
+
+    initialRender();
+  });
+
+  test("Show Popup Modal with Custom connect to wallet", () => {
+    const closeButton = screen.getByText(/Close/i);
+
+    expect(screen.getByText(/Connect Wallet/i)).toBeInTheDocument();
+    expect(closeButton).toBeInTheDocument();
+    expect(
+      screen.getByText(/By connecting a wallet, you agree to Neptune Mutual/i)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Terms & Conditions/i)).toBeInTheDocument();
+    expect(screen.getByText(/Protocol Disclaimer/i)).toBeInTheDocument();
+
+    const customWallet = screen.getByText(/Custom Wallet/i);
+    fireEvent.click(customWallet.parentElement);
+
+    expect(onLogin).toBeCalled();
+  });
+});
