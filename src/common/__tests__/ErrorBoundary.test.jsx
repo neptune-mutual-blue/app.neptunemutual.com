459dcec791b58b5b3f3d79aaa1105a7ef190724b
savinnpm <savin@neptunemutual.com>
2022-09-13 11:55:13 +0545

    improving coverage on ErrorBoundary

--- src/common/__tests__/ErrorBoundary.test.jsx
+++ src/common/__tests__/ErrorBoundary.test.jsx
@@ -1,4 +1,4 @@
-import { act, render } from "@/utils/unit-tests/test-utils";
+import { act, render, screen } from "@/utils/unit-tests/test-utils";
 import { i18n } from "@lingui/core";
 import ErrorBoundary from "@/common/ErrorBoundary";
 
@@ -14,10 +14,87 @@ describe("ErrorBoundary component", () => {
       window.location.hash = "";
     });
     jest.spyOn(console, "error").mockImplementation(() => {});
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
   });
 
   test("should render error if occured", () => {
-    const screen = render(
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+});
+
+describe("simulating different states", () => {
+  beforeEach(() => {
+    i18n.activate("en");
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/error";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/error";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/swap";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/add/v2";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/add/anything";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/remove/v2";
+    render(
+      <ErrorBoundary>
+        <ComponentWithError />
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/Error for testing/i)).toBeInTheDocument();
+  });
+
+  test("should return different state according to path name", () => {
+    window.location.hash = "#/remove/anything";
+    render(
       <ErrorBoundary>
         <ComponentWithError />
       </ErrorBoundary>
