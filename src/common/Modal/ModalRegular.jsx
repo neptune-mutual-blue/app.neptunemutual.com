d61f34e0a62a24140ad44fa11624056933910e34
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-04 10:19:49 +0530

    Added blur to modals

--- src/common/Modal/ModalRegular.jsx
+++ src/common/Modal/ModalRegular.jsx
@@ -21,7 +21,7 @@ export const ModalRegular = ({
     <Portal container={container}>
       <Overlay
         className={classNames(
-          'fixed inset-0 z-40 overflow-y-auto bg-black bg-opacity-15',
+          'fixed inset-0 z-40 overflow-y-auto bg-black bg-opacity-30 backdrop-blur-md',
           overlayClass
         )}
       />
