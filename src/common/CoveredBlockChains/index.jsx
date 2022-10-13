faa1b54c3a5891f5c2a28abe30a3cd6b172e6040
savinnpm <savin@neptunemutual.com>
2022-10-13 13:33:41 +0545

    updated styles

--- src/common/CoveredBlockChains/index.jsx
+++ src/common/CoveredBlockChains/index.jsx
@@ -10,12 +10,12 @@ const CoveredBlockchains = ({ blockChains }) => {
 
   return (
     <div>
-      <h4 className='text-sm font-semibold mt-9'>Covered Blockchain</h4>
+      <h4 className='font-semibold text-h5 mt-9'>Covered Blockchain</h4>
       <div className='mt-2'>
         {blockChains.map((blockChain, index) => (
-          <div className='inline-flex items-center justify-center mr-2 rounded w-fit bg-E6EAEF' key={index}>
+          <div className='inline-flex items-center justify-center mr-2 overflow-hidden rounded w-fit bg-E6EAEF' key={index}>
             {getLogo(blockChain.chainId)}
-            <p className='px-2 py-1 text-sm'>{NetworkNames[blockChain.chainId]}</p>
+            <p className='px-2 py-1 text-sm leading-6'>{NetworkNames[blockChain.chainId]}</p>
           </div>
         ))}
       </div>
