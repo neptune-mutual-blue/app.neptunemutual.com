5e4278b8268c96e4d455547ba743ca40c555e810
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-25 17:48:59 +0530

    Change overflow on modal wrapper

--- src/common/Modal/ModalWrapper.jsx
+++ src/common/Modal/ModalWrapper.jsx
@@ -4,7 +4,7 @@ export const ModalWrapper = ({ className = '', children }) => {
   return (
     <div
       className={classNames(
-        'w-full relative border-[1.5px] border-B0C4DB flex flex-col p-5 text-left align-middle rounded-3xl max-h-full m-auto overflow-scroll',
+        'w-full relative border-[1.5px] border-B0C4DB flex flex-col p-5 text-left align-middle rounded-3xl max-h-full m-auto overflow-auto',
         'sm:p-12 xs:p-8',
         className
       )}
