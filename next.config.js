564d94223a754bc8df9a98198e5de2ea8cc3ccdf
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-26 10:33:53 +0530

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,36 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-const { i18n } = require("./i18n.config");
-const http = require("./http");
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { i18n } = require('./i18n.config')
+const http = require('./http')
 
-module.exports = (phase, { _c }) => {
+/** @type {import('next').NextConfig} */
+const nextConfig = (phase) => {
   return {
     reactStrictMode: true,
-    experimental: {
-      outputStandalone: true,
-    },
+    output: 'standalone',
+    experimental: {},
     eslint: {
-      dirs: ["http", "lib", "src"],
+      dirs: ['http', 'lib', 'src']
     },
     headers: async () => {
       if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return http.headers.development;
+        return http.headers.development
       }
 
-      return http.headers.production;
+      return http.headers.production
     },
     redirects: async () => {
-      return http.redirects;
+      return http.redirects
     },
     i18n,
     webpack: (config) => {
       config.module.rules.push({
         test: /\.po/,
-        use: ["@lingui/loader"],
-      });
+        use: ['@lingui/loader']
+      })
 
-      return config;
-    },
-  };
-};
+      return config
+    }
+  }
+}
+
+module.exports = nextConfig
