2dc5700e0e7f41b3ee7dbb356990a0fd5dd342ae
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-27 21:19:18 +0530

    Refactored middleware and added pragma

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,5 @@
+import { handleBuildManifest, handleGeoBlocking } from '@/src/middleware/handlers'
 import { NextResponse } from 'next/server'
-
-/** @type string */
-const regions = process.env.NEXT_PUBLIC_UNSUPPORTED_REGIONS || ''
-
-const unavailableTo = regions.split(',').filter((x) => !!x)
 
 /**
  *
@@ -11,37 +7,19 @@ const unavailableTo = regions.split(',').filter((x) => !!x)
  * @returns {Promise<Response | undefined> | Response | undefined}
  */
 export function middleware (req) {
-  // if (req.url.includes('buildManifest')) {
-  //   const response = NextResponse.rewrite(new URL('/buildManifest.js', req.url))
-  //   response.headers.set('Access-Control-Allow-Origin', 'null')
-  //   return response
-  // }
+  let response = handleBuildManifest(req)
 
-  const isHTMLPage = typeof req.headers.get('accept') === 'string' && (req.headers.get('accept').includes('text/html') || req.headers.get('accept').includes('application/xhtml+xml'))
-
-  const country = req.geo?.country || ''
-  const isGeoBlocked = country && unavailableTo.indexOf(country) > -1
-
-  if (!isGeoBlocked) {
-    const response = NextResponse.next()
-    response.headers.set('Access-Control-Allow-Origin', 'null')
+  if (response) {
     return response
   }
 
-  if (req.url.includes('buildManifest')) {
-    const response = NextResponse.rewrite(new URL('/buildManifest.js', req.url))
-    response.headers.set('Access-Control-Allow-Origin', 'null')
+  response = handleGeoBlocking(req)
+
+  if (response) {
     return response
   }
 
-  const landingPage = req.nextUrl.clone().pathName === '/unavailable'
-  if (isHTMLPage && !landingPage) {
-    const response = NextResponse.rewrite(new URL('/unavailable', req.url), { status: 451 })
-    response.headers.set('Access-Control-Allow-Origin', 'null')
-    return response
-  }
-
-  const response = NextResponse.next()
+  response = NextResponse.next()
   response.headers.set('Access-Control-Allow-Origin', 'null')
   return response
 }
