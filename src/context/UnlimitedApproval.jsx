667e2efb51d7dd10086c8e411958d0708b056790
savinnpm <savin@neptunemutual.com>
2022-10-29 13:16:27 +0545

    fix lint errors

--- src/context/UnlimitedApproval.jsx
+++ src/context/UnlimitedApproval.jsx
@@ -66,7 +66,7 @@ export const UnlimitedApprovalProvider = ({ children }) => {
       setUnlimitedApproval(value)
       analyticsLogger(() => logUnlimitedApprovalToggled(chainId ?? null, account ?? null, value))
     },
-    [account]
+    [account, chainId]
   )
 
   const getApprovalAmount = useCallback(
