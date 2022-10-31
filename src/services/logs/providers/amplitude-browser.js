a1bc014bd83a485237234ed7bc053331f05e543c
savinnpm <savin@neptunemutual.com>
2022-10-31 10:59:29 +0545

    updated log functions

--- src/services/logs/providers/amplitude-browser.js
+++ src/services/logs/providers/amplitude-browser.js
@@ -132,19 +132,17 @@ const logGesture = (network, name, description, eventData = {}, type = 'swipe') 
   }
 }
 
-const logPageLoad = async (network, account = 'N/A', path) => {
+const logPageLoad = (network, account = 'N/A', path) => {
   init()
 
   const eventName = 'page-load'
 
   try {
-    const result = await amplitude.track(eventName, {
+    amplitude.track(eventName, {
       network,
       account,
       path
-    }).promise
-
-    console.log(result)
+    })
   } catch (e) {
     console.log(`Error in logging ${eventName} event: `, e)
   }
