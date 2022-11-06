7b25d9e7b9575a29333e9b2143712b7a8691981b
flashburst <82953782+flashburst@users.noreply.github.com>
2022-11-06 13:24:10 +0530

    Use resolution sources from cover info

--- src/common/Cover/CoverResolutionSources.jsx
+++ src/common/Cover/CoverResolutionSources.jsx
@@ -2,23 +2,11 @@ import { explainInterval } from '@/utils/formatter/interval'
 import Link from 'next/link'
 import { Trans } from '@lingui/macro'
 import { useCoverStatsContext } from '@/common/Cover/CoverStatsContext'
-import { isValidProduct } from '@/src/helpers/cover'
-import { safeFormatBytes32String } from '@/utils/formatter/bytes32String'
-import { useRouter } from 'next/router'
 import { SecondaryCard } from '@/common/SecondaryCard/SecondaryCard'
 
 export const CoverResolutionSources = ({ children, coverInfo }) => {
-  const router = useRouter()
-  const { productId } = router.query
-  const productKey = safeFormatBytes32String(productId || '')
-  const isDiversified = isValidProduct(productKey)
-  const projectName = isDiversified
-    ? coverInfo?.infoObj.productName
-    : coverInfo?.infoObj.coverName || coverInfo?.infoObj.projectName
   const { reportingPeriod } = useCoverStatsContext()
-
-  const knowledgebase = coverInfo?.infoObj.resolutionSources?.[1] || ''
-  const twitter = coverInfo?.infoObj.resolutionSources?.[0] || ''
+  const resolutionSources = coverInfo?.infoObj.resolutionSources || []
 
   return (
     <div className='col-span-3 row-start-2 lg:col-auto lg:row-start-auto'>
@@ -33,24 +21,19 @@ export const CoverResolutionSources = ({ children, coverInfo }) => {
             </p>
           </div>
           <div className='flex flex-col md:block sm:items-end'>
-            <Link href={knowledgebase}>
-              <a
-                target='_blank'
-                className='block capitalize text-4e7dd9 hover:underline sm:mt-0 md:mt-3'
-                rel='nofollow'
-              >
-                {projectName} Knowledgebase
-              </a>
-            </Link>
-            <Link href={twitter}>
-              <a
-                target='_blank'
-                className='block mt-3 capitalize text-4e7dd9 hover:underline sm:mt-0 md:mt-3'
-                rel='nofollow'
-              >
-                {projectName} Twitter
-              </a>
-            </Link>
+            {resolutionSources.map(source => {
+              return (
+                <Link key={source.uri} href={source.uri}>
+                  <a
+                    target='_blank'
+                    className='block mt-3 capitalize text-4e7dd9 hover:underline sm:mt-0 md:mt-3'
+                    rel='nofollow'
+                  >
+                    {source.text}
+                  </a>
+                </Link>
+              )
+            })}
           </div>
         </div>
 
