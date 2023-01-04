2355a307e89c2e570d3434e44d5d537e7024a5cd
rupak-nm <rupak@neptunemutual.com>
2023-01-04 12:04:43 +0545

    Added sitemap for pages

---
+++ src/utils/fetch/covers.js
@@ -0,0 +1,45 @@
+
+const { getGraphURL } = require('@/src/config/environment')
+
+const query = `
+{
+  covers {
+    coverKey,
+    products {
+      coverKey,
+      productKey
+    }
+  }
+}
+`
+
+const getCoversAndProductsNames = async (networkId) => {
+  if (!networkId) return []
+
+  const url = getGraphURL(networkId)
+
+  if (!url) return []
+
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query
+      })
+    })
+
+    const { data, errors } = await res.json()
+
+    if (!errors) {
+      return data.covers
+    }
+  } catch {}
+
+  return []
+}
+
+export { getCoversAndProductsNames }
---
+++ src/utils/fetch/reports.js
@@ -0,0 +1,43 @@
+
+const { getGraphURL } = require('@/src/config/environment')
+
+const query = `
+{
+  incidentReports {
+    coverKey,
+    productKey,
+    incidentDate
+  }
+}
+`
+
+const getIncidentReports = async (networkId) => {
+  if (!networkId) return []
+
+  const url = getGraphURL(networkId)
+
+  if (!url) return []
+
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query
+      })
+    })
+
+    const { data, errors } = await res.json()
+
+    if (!errors) {
+      return data.incidentReports
+    }
+  } catch {}
+
+  return []
+}
+
+export { getIncidentReports }
