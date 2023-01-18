278e7ba8c2a8807ec29b39361250bbea443c2a83
rupak-nm <rupak@neptunemutual.com>
2023-01-18 10:31:12 +0545

    updated loader svg props

--- src/common/Loader/Loader.jsx
+++ src/common/Loader/Loader.jsx
@@ -12,11 +12,11 @@ export const Loader = ({ className, ...rest }) => {
       {...rest}
       // stroke='#4e7dd9'
     >
-      <g fill='none' fill-rule='evenodd'>
-        <g transform='translate(1 1)' stroke-width='2'>
+      <g fill='none' fillRule='evenodd'>
+        <g transform='translate(1 1)' strokeWidth='2'>
           <circle
             stroke='currentColor'
-            stroke-opacity='.25' cx='18' cy='18' r='18'
+            strokeOpacity='.25' cx='18' cy='18' r='18'
           />
           <path stroke='currentColor' d='M36 18c0-9.94-8.06-18-18-18' />
         </g>
