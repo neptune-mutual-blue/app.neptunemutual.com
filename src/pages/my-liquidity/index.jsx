62070fc9fd37e7351415f35fa4c2457bdafc2b5a
flashburst <82953782+flashburst@users.noreply.github.com>
2023-01-20 13:01:24 +0530

    Refactor meta tags and robots middleware (#840)

--- src/pages/my-liquidity/index.jsx
+++ src/pages/my-liquidity/index.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 
 import { Container } from '@/common/Container/Container'
 import { Hero } from '@/common/Hero'
@@ -18,6 +17,7 @@ import { useCalculateTotalLiquidity } from '@/src/hooks/useCalculateTotalLiquidi
 import { logPageLoad } from '@/src/services/logs'
 import { useEffect } from 'react'
 import { analyticsLogger } from '@/utils/logger'
+import { Seo } from '@/common/Seo'
 
 /* istanbul ignore next */
 export function getStaticProps () {
@@ -58,13 +58,7 @@ export default function MyLiquidity ({ disabled }) {
 
   return (
     <main>
-      <Head>
-        <title>Neptune Mutual Covers</title>
-        <meta
-          name='description'
-          content='Get guaranteed payouts from our parametric cover model. Resolve incidents faster without the need for claims assessment.'
-        />
-      </Head>
+      <Seo />
 
       <Hero>
         <Container className='flex flex-wrap px-2 py-32 min-h-[312px]'>
