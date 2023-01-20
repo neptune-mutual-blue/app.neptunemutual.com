eaf77d451149f6c30083bb179cf778a1e0ff8671
Mark Adriano <mark.adriano15@gmail.com>
2023-01-20 15:34:59 +0800

    Receipt Mobile fixes

--- src/modules/cover/cover-terms/StandardTerms.jsx
+++ src/modules/cover/cover-terms/StandardTerms.jsx
@@ -6,15 +6,13 @@ export const StandardsTerms = ({ className = '' }) => {
   return (
     <div className={className}>
       <h2 className='text-000000 text-para font-bold sm:font-normal sm:text-h1'>Standard Terms and Conditions</h2>
-      <p className='mt-2'>
+      <p className='sm:mt-2'>
         <Trans>
           <a className='underline text-para sm:text-lg text-4e7dd9' href='https://docs.neptunemutual.com/usage/standard-terms-and-conditions' target='_blank' rel='noreferrer'>
             Latest version
           </a> of the Standard Terms and Conditions will apply.
         </Trans>
-
       </p>
-
     </div>
   )
 }
