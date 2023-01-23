3a575ad11ce86f91cb2c09661041ad31e7fc7a6b
rupak-nm <rupak@neptunemutual.com>
2023-01-23 16:16:59 +0545

    Added arbitrum & arrow icons

---
+++ src/icons/TrendUpIcon.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const TrendUpIcon = (props) => (
+  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path d='M16.0713 15.6064V8.53538H9.00022' stroke='white' stroke-width='2' stroke-linecap='round' />
+    <path d='M15.5 9L8.5 16' stroke='white' stroke-width='2' />
+  </svg>
+)
+
+export default TrendUpIcon
