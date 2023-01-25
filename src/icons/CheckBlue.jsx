1b1389592e8ba1594da6c0c2cfba136958eaeff3
Mark Adriano <mark.adriano15@gmail.com>
2023-01-25 20:37:02 +0800

    build fix

--- src/icons/CheckBlue.jsx
+++ src/icons/CheckBlue.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 
 const CheckBlue = (props) => (
-  <svg width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <svg
+    width='14'
+    height='14'
+    viewBox='0 0 14 14'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
     <circle cx='7.00065' cy='7.00065' r='6.66667' fill='#4E7DD9' />
     <path d='M4.33398 7.46699L5.96954 9.00033L9.66732 5.66699' stroke='white' stroke-width='1.33333' stroke-linecap='round' stroke-linejoin='round' />
   </svg>
