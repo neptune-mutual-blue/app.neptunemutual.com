4159f6ef4969efe65dd12443e5d3226d8fd36783
flashburst <82953782+flashburst@users.noreply.github.com>
2023-01-29 20:32:51 +0530

    Dropped amplitude analytics

---
+++ src/services/logs/providers/empty.js
@@ -0,0 +1,187 @@
+const log = async () => {
+
+}
+
+const logPremium = () => {
+
+}
+
+const logAddLiquidityRevenue = () => {
+}
+
+const logButtonClick = () => {
+}
+
+const logGesture = () => {
+}
+
+const logPageLoad = () => {
+}
+
+const logOpenExternalPage = () => {
+}
+
+const logOpenConnectionPopup = () => {
+
+}
+
+const logCloseConnectionPopup = () => {
+
+}
+
+const logWalletConnected = () => {
+
+}
+
+const logWalletDisconnected = () => {
+
+}
+
+const logViewAccountOnExplorer = () => {
+
+}
+
+const logUnlimitedApprovalToggled = () => {
+
+}
+
+const logCoverProductsSearch = () => {
+
+}
+
+const logCoverProductsSort = () => {
+
+}
+
+const logCoverProductsViewChanged = () => {
+
+}
+
+const logPolicyPurchaseRulesAccepted = () => {
+
+}
+
+const logPolicyPurchase = () => {
+
+}
+
+const logAddLiquidityRulesAccepted = () => {
+
+}
+
+const logAddLiquidity = () => {
+
+}
+
+const logRemoveLiquidityModalOpen = () => {
+
+}
+
+const logRemoveLiquidity = () => {
+
+}
+
+const logReportIncidentRulesAccepted = () => {
+
+}
+
+const logIncidentReportStakeApproved = () => {
+
+}
+
+const logIncidentReported = () => {
+
+}
+
+const logIncidentDisputeStakeApproved = () => {
+
+}
+
+const logIncidentDisputed = () => {
+
+}
+
+const logUnstakeReportingRewards = () => {
+
+}
+
+const logClaimCover = () => {
+
+}
+
+const logBondLpTokenApproval = () => {
+
+}
+
+const logBondCreated = () => {
+
+}
+
+const logBondClaimed = () => {
+
+}
+
+const logCoverProductRulesDownload = () => {
+
+}
+
+const logStakingPoolDepositPopupToggled = () => {
+
+}
+
+const logStakingPoolDeposit = () => {
+
+}
+
+const logStakingPoolCollectPopupToggled = () => {
+
+}
+
+const logStakingPoolWithdraw = () => {
+
+}
+
+const logStakingPoolWithdrawRewards = () => {
+
+}
+
+export {
+  log,
+  logAddLiquidityRevenue,
+  logPremium,
+  logWalletConnected,
+  logButtonClick,
+  logGesture,
+  logPageLoad,
+  logOpenExternalPage,
+  logOpenConnectionPopup,
+  logCloseConnectionPopup,
+  logWalletDisconnected,
+  logViewAccountOnExplorer,
+  logUnlimitedApprovalToggled,
+  logCoverProductsSearch,
+  logCoverProductsSort,
+  logCoverProductsViewChanged,
+  logPolicyPurchaseRulesAccepted,
+  logPolicyPurchase,
+  logAddLiquidityRulesAccepted,
+  logAddLiquidity,
+  logRemoveLiquidityModalOpen,
+  logRemoveLiquidity,
+  logReportIncidentRulesAccepted,
+  logIncidentReportStakeApproved,
+  logIncidentReported,
+  logIncidentDisputeStakeApproved,
+  logIncidentDisputed,
+  logUnstakeReportingRewards,
+  logClaimCover,
+  logBondLpTokenApproval,
+  logBondCreated,
+  logBondClaimed,
+  logCoverProductRulesDownload,
+  logStakingPoolDepositPopupToggled,
+  logStakingPoolDeposit,
+  logStakingPoolCollectPopupToggled,
+  logStakingPoolWithdraw,
+  logStakingPoolWithdrawRewards
+}
