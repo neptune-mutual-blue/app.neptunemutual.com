53f67d58d8c8e9c26e9961a22738f515ee96cfd4
flashburst <82953782+flashburst@users.noreply.github.com>
2023-01-31 18:56:58 +0530

    Removed unnecessary parameters

--- src/hooks/useRegisterToken.jsx
+++ src/hooks/useRegisterToken.jsx
@@ -1,20 +1,18 @@
 import { registerToken } from '@/lib/connect-wallet/utils/wallet'
 import { API_BASE_URL } from '@/src/config/constants'
-import { useNetwork } from '@/src/context/Network'
 import { useWeb3React } from '@web3-react/core'
 
 export const useRegisterToken = () => {
-  const { networkId } = useNetwork()
   const { account } = useWeb3React()
 
   const register = (address, symbol, decimals = 18) => {
-    if (!networkId || !account) return
+    if (!account) return
 
     const image = symbol
       ? `${API_BASE_URL}images/tokens/${symbol.toLowerCase()}.svg`
       : undefined
 
-    registerToken(networkId, address, symbol, decimals, image)
+    registerToken(address, symbol, decimals, image)
       .then(console.log)
       .catch(console.error)
   }
