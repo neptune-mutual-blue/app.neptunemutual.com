ee23f82fafb7c3bca8c768e9d23b241bf31f07ba
flashburst <82953782+flashburst@users.noreply.github.com>
2023-02-10 09:02:29 +0530

    Fix cookie popup to stop showing if user declines

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,40 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-// Hook
-export function useLocalStorage (key, initialValue) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    if (!process.browser) return
+export const useLocalStorage = (key, initialState) => {
+  const [state, setState] = useState(initialState)
 
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error)
-      return initialValue
+  useEffect(() => {
+    const item = localStorage.getItem(key)
+
+    if (item) {
+      setState(parse(item))
     }
-  })
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
-  const setValue = (value) => {
-    if (!process.browser) return
+  }, [key])
 
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value
-      // Save state
-      setStoredValue(valueToStore)
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error)
+  useEffect(() => {
+    if (typeof state === 'undefined') {
+      localStorage.removeItem(key)
+      return
     }
+
+    localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
+}
+
+const parse = (value) => {
+  try {
+    return JSON.parse(value)
+  } catch (_a) {
+    return value
   }
-  return [storedValue, setValue]
 }
