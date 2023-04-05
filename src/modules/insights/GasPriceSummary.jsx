4f910d9934de4567da551b36311e41aefa08aca5
Ankit Kumar Karna <ankitkarna.dev@gmail.com>
2023-04-05 11:07:50 +0545

    Added null check on finding max-values

--- src/modules/insights/GasPriceSummary.jsx
+++ src/modules/insights/GasPriceSummary.jsx
@@ -27,7 +27,7 @@ const GasPriceSummary = ({ data, loading }) => {
 
             return (
               <div key={chainGasSummary.chainId} className='-mr-6'>
-                <GaugeChart min={0} max={maxValues[chainGasSummary.chainId].find((x) => x >= +avgGasPrice.gweiNumeric) ?? Math.ceil(maxGasPrice.gweiNumeric)} value={avgGasPrice.gweiNumeric} />
+                <GaugeChart min={0} max={maxValues[chainGasSummary.chainId]?.find((x) => x >= +avgGasPrice.gweiNumeric) ?? Math.ceil(maxGasPrice.gweiNumeric)} value={avgGasPrice.gweiNumeric} />
                 <div className='px-8 -ml-3 text-center -mt-14 first-of-type:pl-3'>
                   <div className='text-md leading-7.5 mb-1.5 font-bold relative z-1' title={avgGasPrice.gweiLong}>{avgGasPrice.gwei}</div>
                   <div className='text-sm font-semibold leading-5'>{chainGasSummary.nickName !== 'Mainnet' ? chainGasSummary.nickName : 'Ethereum'}</div>
