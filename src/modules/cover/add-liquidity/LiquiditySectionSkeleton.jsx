d237b8150871c3727eb8fac9a10cf2ef7ca21ec4
acharyarupak391 <acharyarupak391@gmail.com>
2023-05-18 17:03:34 +0545

    Added loading skeletons for:   - Report dispute page   - Resolved reports table   - Purchase policy page   - Provide Liquidity page

---
+++ src/modules/cover/add-liquidity/LiquiditySectionSkeleton.jsx
@@ -0,0 +1,29 @@
+import { Container } from '@/common/Container/Container'
+import { Skeleton } from '@/common/Skeleton/Skeleton'
+import { HeroSkeleton } from '@/modules/reporting/ReportDetailsSkeleton'
+
+const LiquiditySectionSkeleton = () => (
+  <>
+    <HeroSkeleton />
+
+    <div className='pt-12 pb-24 border-t border-t-B0C4DB'>
+      <Container className=''>
+        <Skeleton className='h-8 mb-6 w-96' />
+
+        <Skeleton className='w-full h-4' />
+        <Skeleton className='w-40 h-4 mt-1.5' />
+
+        <Skeleton className='h-8 mt-6 mb-12 w-36' />
+
+        <Skeleton className='w-full h-4' />
+        <Skeleton className='w-full h-4 mt-1.5' />
+        <Skeleton className='w-full h-4 mt-1.5' />
+        <Skeleton className='w-full h-4 mt-1.5' />
+        <Skeleton className='w-full h-4 mt-1.5' />
+        <Skeleton className='h-4 mt-1.5 w-96' />
+      </Container>
+    </div>
+  </>
+)
+
+export { LiquiditySectionSkeleton }
