8d1139e7ec1ce1fc4ff27e596af457e4cf9d8906
acharyarupak391 <acharyarupak391@gmail.com>
2024-01-30 13:35:29 +0545

    Add new banners for Arbitrum, BSC, and Ethereum and updated link

--- src/modules/home/AnnouncementBanner.jsx
+++ src/modules/home/AnnouncementBanner.jsx
@@ -1,7 +1,26 @@
+import { useNetwork } from '@/src/context/Network'
+
 export default function AnnouncementBanner () {
-  const imageSrc = '/new-year-banner.jpg'
-  const imageAlt = 'New year rewards'
-  const linkHref = 'https://community.neptunemutual.com/t/enjoy-110-cashback-this-new-year/279'
+  const imageAlt = 'Earn fees from NPM trading'
+
+  const { networkId } = useNetwork()
+
+  const data = {
+    1: {
+      image: '/images/banners/Ethereum-Banner-Desktop.webp',
+      link: 'https://www.sushi.com/pool/137%3A0x0e3eaef09dfe55824a3cda7146a387af261d7824'
+    },
+    42161: {
+      image: '/images/banners/Arbitrum-Banner-Desktop.webp',
+      link: 'https://www.sushi.com/pool/137%3A0x0e3eaef09dfe55824a3cda7146a387af261d7824'
+    },
+    56: {
+      image: '/images/banners/BSC-Banner-Desktop.webp',
+      link: 'https://www.sushi.com/pool/137%3A0x0e3eaef09dfe55824a3cda7146a387af261d7824'
+    }
+  }
+
+  if (!data[networkId]) { return null }
 
   return (
     <div
@@ -9,9 +28,12 @@ export default function AnnouncementBanner () {
       data-testid='announcement-banner'
     >
       <div className='relative'>
-
-        <a href={linkHref} target='_blank' rel='noreferrer'>
-          <img className='object-cover h-64 max-w-full overflow-hidden sm:h-96 rounded-xl' src={imageSrc} alt={imageAlt} />
+        <a href={data[networkId].link} target='_blank' rel='noreferrer'>
+          <img
+            className='object-cover object-center h-64 max-w-full overflow-hidden sm:h-96 rounded-xl'
+            src={data[networkId].image}
+            alt={imageAlt}
+          />
         </a>
       </div>
 
