c553ebac6e01fc704a799f231a56c81e82bd59e4
keyrunpay <tokeyrun@gmail.com>
2024-06-17 17:25:56 +0545

    Reassurance ration changed to Utilization ratio

--- src/common/Cover/MyLiquidity/MyLiquidityCoverCard.jsx
+++ src/common/Cover/MyLiquidity/MyLiquidityCoverCard.jsx
@@ -12,9 +12,7 @@ import { Divider } from '@/common/Divider/Divider'
 import { OutlinedCard } from '@/common/OutlinedCard/OutlinedCard'
 import { ProgressBar } from '@/common/ProgressBar/ProgressBar'
 import { getCoverImgSrc } from '@/src/helpers/cover'
-import { useMyLiquidityInfo } from '@/src/hooks/useMyLiquidityInfo'
 import {
-  sumOf,
   toBN
 } from '@/utils/bn'
 import { formatPercent } from '@/utils/formatter/percent'
@@ -30,14 +28,11 @@ export const MyLiquidityCoverCard = ({
   coverData
 }) => {
   const router = useRouter()
-  const { info } = useMyLiquidityInfo({ coverKey })
 
   const isDiversified = coverData?.supportsProducts
   const projectName = coverData.coverInfoDetails.coverName || coverData.coverInfoDetails.projectName
 
-  const reassurancePercent = toBN(info.totalReassurance)
-    .dividedBy(sumOf(info.totalLiquidity, info.totalReassurance))
-    .decimalPlaces(2)
+  const utilizationPercent = toBN(coverData?.utilizationRatio)
 
   return (
     <OutlinedCard className='p-6 bg-white' type='link'>
@@ -87,14 +82,14 @@ export const MyLiquidityCoverCard = ({
       {/* Stats */}
       <div className='flex justify-between px-1 text-sm'>
         <span className='uppercase'>
-          <Trans>Reassurance Ratio</Trans>
+          <Trans>Utilization Ratio</Trans>
         </span>
         <span className='font-semibold text-right' data-testid='assurance'>
-          {formatPercent(reassurancePercent, router.locale)}
+          {formatPercent(utilizationPercent, router.locale)}
         </span>
       </div>
       <div className='mt-2 mb-4'>
-        <ProgressBar value={reassurancePercent?.toNumber()} />
+        <ProgressBar value={utilizationPercent?.toNumber()} />
       </div>
 
       <div className='flex items-center gap-2'>
